a6fc844a1f6477b8e239a8b5575132f8683f4d81
Josh Bendavid <Josh.Bendavid@cern.ch>
2022-02-04 11:57:22 +0000

    resynchronize with cmgtools-lite repository and shorten namespace name

--- wremnants/include/defines.h
+++ wremnants/include/defines.h
@@ -5,7 +5,7 @@
 #include <ROOT/RDataFrame.hxx>
 #include <ROOT/RDF/RInterface.hxx>
 
-namespace wremnants {
+namespace wrem {
 
 using Vec_b = ROOT::VecOps::RVec<bool>;
 using Vec_d = ROOT::VecOps::RVec<double>;
@@ -13,23 +13,22 @@ using Vec_f = ROOT::VecOps::RVec<float>;
 using Vec_i = ROOT::VecOps::RVec<int>;
 using Vec_ui = ROOT::VecOps::RVec<unsigned int>;
 
-typedef enum {isoTrigPlus=0, isoTrigMinus, isoNotrig, noisoTrigPlus, noisoTrigMinus, noisoNotrig, antiisoTrigPlus, antiisoTrigMinus, antiisoNotrig, trackingAndReco, isoTrigPlusStepOfTnp, isoNotrigStepOfTnP, tracking, altTracking, reco, altReco, global, isoOnly, antiisoOnly, isoNotrigOnly, antiisoNotrigOnly} ScaleFactorType; // to use product
+typedef enum {isoTrigPlus=0, isoTrigMinus, isoNotrig, noisoTrigPlus, noisoTrigMinus, noisoNotrig, antiisoTrigPlus, antiisoTrigMinus, antiisoNotrig, trackingReco, isoTrigPlusStepOfTnp, isoNotrigStepOfTnP, tracking, altTracking, reco, altReco, global, isoOnly, antiisoOnly, isoNotrigOnly, antiisoNotrigOnly} ScaleFactorType; // to use product
 typedef enum {BToH=0, BToF, B, C, D, E, F, F_preVFP, F_postVFP, G, H, GToH} DataEra;  // keep sorted with postVFP after preVFP (BToH is an exception but we don't use it anyway, leave it as 0)
 typedef enum {MC=0, Data} DataType;
 
 bool isOddEvent(ULong64_t evt) {
 
-  return (evt%2) ? 1 : 0;       
+  return (evt%2) ? 1 : 0;
 
 }
 
 bool isEvenEvent(ULong64_t evt) {
 
-  return (evt%2) ? 0 : 1;       
+  return (evt%2) ? 0 : 1;
 
 }
 
 }
-
 
 #endif
