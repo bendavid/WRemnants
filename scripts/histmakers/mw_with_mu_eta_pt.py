c4a78d4224db0e73cceade451cf57f21c4898140
Josh Bendavid <Josh.Bendavid@cern.ch>
2022-02-04 01:53:45 +0000

    implementation of PDF uncertainties and partial implementation of efficiency uncertainties

--- scripts/histmakers/mw_with_mu_eta_pt.py
+++ scripts/histmakers/mw_with_mu_eta_pt.py
@@ -97,10 +97,30 @@ def build_graph(df, dataset):
 
         df = df.Define("nominal_weight", "weight*weight_pu*weight_fullMuonSF*weight_newMuonPrefiringSF*weight_tnpRecoSF*weight_tnpTrackingSF")
 
-        nominal_weight_col = "nominal_weight"
-
-        nominal = df.HistoBoost("nominal", nominal_axes, [*nominal_cols, nominal_weight_col])
+        nominal = df.HistoBoost("nominal", nominal_axes, [*nominal_cols, "nominal_weight"])
         results.append(nominal)
+
+        # slice 101 elements starting from 0 and clip values at += 10.0
+        df = df.Define("pdfWeights_tensor", "wremnants::clip_tensor(wremnants::vec_to_tensor_t<float, 101>(LHEPdfWeight), 10.)")
+
+        pdfNNPDF31 = df.HistoBoost("pdfNNPDF31", nominal_axes, [*nominal_cols, "pdfWeights_tensor"])
+        results.append(pdfNNPDF31)
+
+        # slice 2 elements starting from 101
+        df = df.Define("pdfWeightsAS_tensor", "wremnants::vec_to_tensor_t<double, 2>(LHEPdfWeight, 101)")
+
+        alphaS002NNPDF31 = df.HistoBoost("alphaS002NNPDF31", nominal_axes, [*nominal_cols, "pdfWeightsAS_tensor"])
+        results.append(alphaS002NNPDF31)
+
+        #TODO check that I actually have a consistent version of functions.h and functionsWMass.h
+        #TODO convert _get_fullMuonSFvariation_splitIso to produce a tensor natively
+        df = df.Define("effStatTnP_tensor", "Eigen::TensorFixedSize<double, Eigen::Sizes<1248>> res = (nominal_weight/weight_fullMuonSF)*wremnants::vec_to_tensor_t<double, 1248>(wremnants::_get_fullMuonSFvariation_splitIso(624, goodMuons_pt0 ,goodMuons_eta0,goodMuons_charge0,-1,-1,eraVFP,passIso)); return res;")
+
+        effStatTnP = df.HistoBoost("effStatTnP", nominal_axes, [*nominal_cols, "effStatTnP_tensor"])
+        results.append(effStatTnP)
+
+
+
 
 
 
