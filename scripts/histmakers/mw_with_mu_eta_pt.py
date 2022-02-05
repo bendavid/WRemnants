7bfeca0f52f1768f99581e2798c0991f6d62e6fa
Kenneth Long <kdlong@hep.wisc.edu>
2022-02-05 15:52:32 +0000

    Add dummy muon scale using mass weights
    Fix unintended changes

--- scripts/histmakers/mw_with_mu_eta_pt.py
+++ scripts/histmakers/mw_with_mu_eta_pt.py
@@ -20,8 +20,6 @@ import narf
 import wremnants
 import hist
 import lz4.frame
-import numba
-
 
 ROOT.wrem.initializeScaleFactors(wremnants.data_dir, wremnants.data_dir + "/testMuonSF/scaleFactorProduct_28Oct2021_nodz_dxybs_genMatchDR01.root")
 
@@ -161,6 +159,14 @@ def build_graph(df, dataset):
 
             massWeight = df.HistoBoost("massWeight", nominal_axes, [*nominal_cols, "massWeight_tensor"])
             results.append(massWeight)
+
+            netabins = 4
+            df = df.Define("muonScaleDummy4Bins2e4", f"dummyScaleFromMassWeights<{netabins}, 21>(massWeight_tensor, goodMuons_eta0, 2.e-4)")
+            scale_etabins_axis = hist.axis.Regular(4, -2.4, 2.4, name="scaleEtaSlice", underflow=False, overflow=False)
+            dummyMuonScaleSyst = df.HistoBoost("muonScaleSyst", nominal_axes, [*nominal_cols, "muonScaleDummy4Bins2e4"], 
+                tensor_axes=[down_up_axis, scale_etabins_axis])
+            results.append(dummyMuonScaleSyst)
+
 
     return results, weightsum
 
---
+++ wremnants/include/muonCorr.h
@@ -0,0 +1,30 @@
+#include <eigen3/Eigen/Dense>
+#include <eigen3/unsupported/Eigen/CXX11/Tensor>
+
+double scaleWeight(double weight, double scale) {
+    return std::exp(scale*std::log(std::abs(weight))*std::copysign(1., weight));
+}
+
+template <size_t ETABINS, size_t T>
+Eigen::TensorFixedSize<double, Eigen::Sizes<2, ETABINS>> dummyScaleFromMassWeights(
+    Eigen::TensorFixedSize<double, Eigen::Sizes<T>>& weights, double eta, double scale, bool isW=true) {
+    const double refMass = isW ? 80351.81229 : 91153.50974;
+    const size_t centralIdx = isW ? 10 : 11;
+    const double scaleMeV = refMass*scale;
+    const int step10MeV = static_cast<int>(scaleMeV/10)+1;
+    if (centralIdx+step10MeV > T-1)
+        throw std::out_of_range("Maximum allowed range for momentum scale uncertainty is 100 MeV!");
+    // Find weight (10 MeV steps) closest, but smaller than desired scale, then scale down
+    const double scaleFac = scaleMeV/(10*step10MeV);
+
+    Eigen::TensorFixedSize<double, Eigen::Sizes<2, ETABINS>> outWeights;
+    outWeights.setConstant(1.);
+
+    const double etaStep = 2*2.4/ETABINS;
+    size_t ieta = (std::clamp(eta, -2.4, 2.4)+2.4)/etaStep;
+
+    // Down weight, then up weight
+    outWeights(0, ieta) = scaleWeight(weights[centralIdx-step10MeV], scaleFac);
+    outWeights(1, ieta) = scaleWeight(weights[centralIdx+step10MeV], scaleFac);
+    return outWeights;
+}
