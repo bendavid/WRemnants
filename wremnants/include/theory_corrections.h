d85431bb39610da6e07f60d0ada0895d6661ef45
Kenneth Long <kdlong@hep.wisc.edu>
2022-02-07 11:10:49 +0000

    Return tensor by value, add overflow to hist

--- wremnants/include/theory_corrections.h
+++ wremnants/include/theory_corrections.h
@@ -8,15 +8,18 @@ namespace wrem {
 
 template <typename T>
 class ScetlibCorrectionsHelper {
+
+typedef typename T::storage_type::value_type::tensor_t tensor_t;
+
 public:
     ScetlibCorrectionsHelper(T&& corrections) :
         correctionHist_(std::make_shared<const T>(std::move(corrections))) {}
 
-    const auto& operator() (float mass, float y, float pt) {
-        size_t massbin = correctionHist_->template axis<0>().index(mass);
-        size_t ybin = correctionHist_->template axis<1>().index(y);
-        size_t ptbin = correctionHist_->template axis<2>().index(pt);
-        return correctionHist_->at(massbin, ybin, ptbin);
+    tensor_t operator() (double mass, double y, double pt) {
+        auto massbin = correctionHist_->template axis<0>().index(mass);
+        auto ybin = correctionHist_->template axis<1>().index(y);
+        auto ptbin = correctionHist_->template axis<2>().index(pt);
+        return correctionHist_->at(massbin, ybin, ptbin).data();
     }
 private:
     std::shared_ptr<const T> correctionHist_;
