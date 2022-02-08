da434d0bf0f4dfbf3e16b75460584a43ac7dabf2
Josh Bendavid <Josh.Bendavid@cern.ch>
2022-02-08 23:58:13 +0000

    remove unused functions and migrate those still needed to utils.h

--- wremnants/__init__.py
+++ wremnants/__init__.py
@@ -3,16 +3,15 @@ import pathlib
 
 ROOT.gInterpreter.AddIncludePath(f"{pathlib.Path(__file__).parent}/include/")
 
-ROOT.gInterpreter.Declare('#include "functions.h"')
 ROOT.gInterpreter.Declare('#include "muonCorr.h"')
 ROOT.gInterpreter.Declare('#include "theoryTools.h"')
-ROOT.gInterpreter.Declare('#include "functionsWMass.h"')
 ROOT.gInterpreter.Declare('#include "pileupWeights.h"')
 ROOT.gInterpreter.Declare('#include "utils.h"')
 
 from .datasets import datasets2016
 
 from .muon_prefiring import make_muon_prefiring_helpers
+from .muon_efficiencies import make_muon_efficiency_helpers
 from .scetlib_corrections import makeScetlibCorrHelper
 
 data_dir = f"{pathlib.Path(__file__).parent}/data/"
