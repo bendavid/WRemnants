81a691d5007f76a8781e3804b08f36430573e3e4
Kenneth Long <kdlong@hep.wisc.edu>
2022-02-08 00:19:17 +0000

    Add a kind of sloppy plotting notebook example

---
+++ wremnants/histselections.py
@@ -0,0 +1,29 @@
+from . import boostHistHelpers as hh
+import hist
+import numpy as np
+
+def fakeHistABCD(h):
+    return hh.multiplyHists(
+        hh.divideHists(h[{"passIso" : True, "passMT" : False}], 
+            h[{"passIso" : False, "passMT" : True}],
+                where=h[{"passIso" : False, "passMT" : True}].values()>1),
+        h[{"passIso" : False, "passMT" : False}], 
+    )
+
+def fakeHistIsoRegion(h, scale=1.):
+    return h[{"iso" : 0.3j, "mt" : hist.rebin(10)}]*scale
+
+def signalHistWmass(h, charge=None):
+    sel = {"passIso" : 1, "passMT" : 1}
+    if charge in [-1, 1]:
+        sel.update({"charge" : -1j if charge < 0 else 1j})
+    return h[sel]
+
+def signalHistLowPileupW(h):
+    return h[{"iso" : 0.j, "mt" : hist.rebin(10)}]
+
+def unrolledHist(h, obs=["pt", "eta"]):
+    bins = np.multiply(*[a.size for a in h.axes[:2]])
+    newh = hist.Hist(hist.axis.Regular(bins, 0, bins), storage=hist.storage.Weight())
+    newh[...] = np.ravel(h.project(*obs))
+    return newh
