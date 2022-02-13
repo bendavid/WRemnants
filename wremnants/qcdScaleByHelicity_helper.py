803f7f714a10a60aa62e02fe79c6a5e30f65637a
Josh Bendavid <Josh.Bendavid@cern.ch>
2022-02-13 05:17:09 +0000

    fixes and improvements for theory weights

--- wremnants/qcdScaleByHelicity_helper.py
+++ wremnants/qcdScaleByHelicity_helper.py
@@ -9,16 +9,16 @@ data_dir = f"{pathlib.Path(__file__).parent}/data/"
 
 def makeQCDScaleByHelicityHelper(input_path=f"{data_dir}/angularCoefficients"):
     charge_axis = hist.axis.Regular(
-        2, -2, 2, name="charge", underflow=False, overflow=False
+        2, -2, 2, name="chargeVgen", underflow=False, overflow=False
     )
     mur_axis = hist.axis.Regular(
-        3, 0, 3, name="muRDownNomUp", underflow=False, overflow=False
+        3, -1.5, 1.5, name="muRDownNomUp", underflow=False, overflow=False
     )
     muf_axis = hist.axis.Regular(
-        3, 0, 3, name="muFDownNomUp", underflow=False, overflow=False
+        3, -1.5, 1.5, name="muFDownNomUp", underflow=False, overflow=False
     )
-    coeff_axis = hist.axis.Regular(
-        9, 0, 9, name="coeff", overflow=False, underflow=False
+    coeff_axis = hist.axis.Integer(
+        0, 9, name="coeff", overflow=False, underflow=False
     )
     f = uproot.open(f"{input_path}/fractions_minus_2022.root")
     nom = f["unpol_minus_nominal"].to_hist()
@@ -44,5 +44,9 @@ def makeQCDScaleByHelicityHelper(input_path=f"{data_dir}/angularCoefficients"):
 
                     rthist = f[f"{coeff}_{charge}_{name}"].to_hist()
                     corrh.view(flow=True)[...,i,j,l,k] = rthist.values(flow=True)
+
+    #should be uniformly 1.0 for 1+cos^2 theta term
+    corrh.values(flow=True)[...,0] = 1.0
+    corrh.variances(flow=True)[...,0] = 0.0
 
     return makeCorrectionsTensor(corrh, ROOT.wrem.QCDScaleByHelicityCorrectionsHelper, tensor_rank=3)
