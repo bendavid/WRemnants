7ea2b68b02b7e4cd696826cc14f886c262e03213
Kenneth Long <kdlong@hep.wisc.edu>
2022-02-17 00:30:32 +0000

    Dump sloppy plotting stuff

--- wremnants/syst_tools.py
+++ wremnants/syst_tools.py
@@ -15,12 +15,8 @@ def scale_helicity_hist_to_variations(scale_hist):
     # difference between a given scale and the nominal, plus the sum
     # this emulates the "weight if idx else nominal" logic and corresponds to the decorrelated
     # variations
-    if scale_hist.name is None:
-        out_name = "scale_helicity_variations"
-    else:
-        out_name = scale_hist.name + "_variations"
-
-    scale_variation_hist = hist.Hist(*scale_hist.axes, storage = scale_hist._storage_type(), name = out_name,
+    name = "variations" if not scale_hist.name else f"{scale_hist.name}_variations"
+    scale_variation_hist = hist.Hist(*scale_hist.axes, storage = scale_hist._storage_type(), name = name,
                 data = scale_hist.view(flow=True) - nom_scale_hist.view(flow=True) + nom_hist.view(flow=True)[..., np.newaxis, np.newaxis, np.newaxis, np.newaxis, np.newaxis])
 
     return scale_variation_hist
