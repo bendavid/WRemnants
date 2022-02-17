f48bdfa54ab3f1d0c199d07df2519bb90b33b326
Kenneth Long <kdlong@hep.wisc.edu>
2022-02-17 00:30:32 +0000

    Dump sloppy plotting stuff

--- wremnants/plotTools.py
+++ wremnants/plotTools.py
@@ -4,8 +4,8 @@ from wremnants import boostHistHelpers as hh
 from wremnants import histselections as sel
 hep.style.use(hep.style.ROOT)
 
-def makePlotWithRatio(obs, histInfo, stackedProcs, label="nominal", unstacked=None, xlabel="", ylabel="Events/bin", 
-                rrange=[0.9, 1.1], scale=8.5e6, action=None):
+def makeStackPlotWithRatio(obs, histInfo, stackedProcs, label="nominal", unstacked=None, xlabel="", ylabel="Events/bin", 
+                rrange=[0.9, 1.1], scale=8.5e6):
     width=3 if "unrolled" in obs else 1
     fig = plt.figure(figsize=(8*width,8))
     ax1 = fig.add_subplot(4, 1, (1, 3)) 
@@ -29,7 +29,9 @@ def makePlotWithRatio(obs, histInfo, stackedProcs, label="nominal", unstacked=No
     )
     
     if unstacked:
-        unstack = op(histInfo[unstacked][label])
+        if not ratioHist:
+            ratioHist = label
+        unstack = op(histInfo[unstacked][ratioHist])
         hep.histplot(
             unstack,
             yerr=True, 
@@ -51,12 +53,55 @@ def makePlotWithRatio(obs, histInfo, stackedProcs, label="nominal", unstacked=No
     ax2.set_xlabel(xlabel)
     ax1.set_ylabel(ylabel)
     ax1.set_xticklabels([])
-    xrange = [stack[0].axes[0].edges[0], stack[0].axes[0].edges[stack[0].axes[0].size-1]]
-    ax1.set_xlim(xrange)
-    ax2.set_xlim(xrange)
+    xlim = [stack[0].axes[0].edges[0], stack[0].axes[0].edges[stack[0].axes[0].size-1]]
+    ax1.set_xlim(xlim)
+    ax2.set_xlim(xlim)
     ax2.set_ylabel("data/pred.", fontsize=22)
     ax2.set_ylim(rrange)
     ax1.set_ylim([0, scale])
     handles, labels = ax1.get_legend_handles_labels()
     ax1.legend(reversed(handles), reversed(labels), prop={'size' : 20*(0.7 if width == 1 else 1.3)}, ncol=2, loc='upper right')
     return fig
+
+def makePlotsWithRatioToRef(hists, labels, colors, xlabel="", ylabel="Events/bin", 
+                rrange=[0.9, 1.1], scale=8.5e6):
+    # For unrolled hists, maybe not the most elegant way but it kinda works
+    width = 1 if hists[0].axes[0].size < 500 else 3
+    fig = plt.figure(figsize=(8*width,8))
+    ax1 = fig.add_subplot(4, 1, (1, 3)) 
+    ax2 = fig.add_subplot(4, 1, 4) 
+    
+    hep.histplot(
+        hists,
+        histtype="step",
+        color=colors,
+        label=labels,
+        stack=False,
+        ax=ax1
+    )
+    
+    if len(hists) > 1:
+        hep.histplot(
+                [hh.divideHists(h, hists[0]) for h in hists[1:]],
+            histtype="step",
+            color=colors[1:],
+            label=labels[1:],
+            yerr=True,
+            stack=False,
+            ax=ax2
+        )
+        
+    ax1.set_xlabel("")
+    ax2.set_xlabel(xlabel)
+    ax1.set_ylabel(ylabel)
+    ax1.set_xticklabels([])
+    xlim = [hists[0].axes[0].edges[0], hists[0].axes[0].edges[hists[0].axes[0].size-1]]
+    ax1.set_xlim(xlim)
+    ax2.set_xlim(xlim)
+    ax2.set_ylabel("data/pred.", fontsize=22)
+    ax2.set_ylim(rrange)
+    ax1.set_ylim([0, scale])
+    handles, labels = ax1.get_legend_handles_labels()
+    ax1.legend(reversed(handles), reversed(labels), prop={'size' : 20*(0.7 if width == 1 else 1.3)}, ncol=2, loc='upper right')
+    return fig
+
--- wremnants/theory_tools.py
+++ wremnants/theory_tools.py
@@ -67,3 +67,36 @@ def moments_to_angular_coeffs(hist_moments_scales):
 
 
     return hist_coeffs_scales
+
+def qcdByHelicityLabels():
+    coeffs = ["const"]+[f"a{i}" for i in range(8)]
+    scaleVars = ["muRmuF", "muR", "muF"]
+    return  [f"{var}_{coeff}{t}" for var in scaleVars for t in ["Up", "Down"] for coeff in coeffs]
+
+def qcdScaleNames():
+    # Exclude central and extreme variations
+    shifts = ["muRmuFDown", "muRDown", "", "muFDown", "", "muFUp", "muRUp", "", "muRmuFUp"]
+    return ["_".join(["QCDscale", s]) if s != "" else s for s in shifts]
+
+def massWeightNames(matches=None):
+    central=10
+    nweights=21
+    names = [f"massShift{int(abs(central-i)*10)}MeV{'Down' if i < central else 'Up'}" for i in range(nweights)]
+    # If name is "" it won't be stored
+    return [x if not matches or any(y in x for y in matches) else "" for x in names]
+
+def pdfNames(cardTool, pdf, skipFirst=True):
+    size = 101
+    names = cardTool.mirrorNames(f"pdf{{i}}{pdf}", size)
+    if skipFirst:
+        names[0] = ""
+        names[size] = ""
+    # TODO: This is probably not needed anymore, check with low PU
+    if False and pdf == "NNPDF31":
+        names[size-2] = "pdfAlphas002Up"
+        names[size-1] = "pdfAlphas002Down"
+        # Drop the mirrored alphaS variations
+        names[size*2-2] = ""
+        names[size*2-1] = ""
+    return names
+
