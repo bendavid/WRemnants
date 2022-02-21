35593391290455d78328ab5c7b2935612c800f3f
Kenneth Long <kdlong@hep.wisc.edu>
2022-02-21 10:16:00 +0000

    Fix nominal weight treatment in muon corr

--- wremnants/include/muonCorr.h
+++ wremnants/include/muonCorr.h
@@ -8,11 +8,11 @@
 namespace wrem {
 
 double scaleWeight(double weight, double scale) {
-    return std::exp(scale*std::log(std::abs(weight))*std::copysign(1., weight));
+    return std::exp(scale*std::log(std::abs(weight)))*std::copysign(1., weight);
 }
 
 template <size_t ETABINS, size_t T>
-Eigen::TensorFixedSize<double, Eigen::Sizes<2, ETABINS>> dummyScaleFromMassWeights(
+Eigen::TensorFixedSize<double, Eigen::Sizes<2, ETABINS>> dummyScaleFromMassWeights(double nominal_weight,
     Eigen::TensorFixedSize<double, Eigen::Sizes<T>>& weights, double eta, double scale, bool isW=true) {
     const double refMass = isW ? 80351.81229 : 91153.50974;
     const size_t centralIdx = 10;
@@ -20,18 +20,17 @@ Eigen::TensorFixedSize<double, Eigen::Sizes<2, ETABINS>> dummyScaleFromMassWeigh
     const int step10MeV = std::floor(scaleMeV/10.)+1;
     if (centralIdx-step10MeV < 0)
         throw std::out_of_range("Maximum allowed range for momentum scale uncertainty is 100 MeV!");
-    // Find weight (10 MeV steps) closest, but smaller than desired scale, then scale down
     const double scaleFac = scaleMeV/(10.*step10MeV);
 
     Eigen::TensorFixedSize<double, Eigen::Sizes<2, ETABINS>> outWeights;
-    outWeights.setConstant(1.);
+    outWeights.setConstant(nominal_weight);
 
     const double etaStep = 2*2.4/ETABINS;
     size_t ieta = (std::clamp(eta, -2.4, 2.4)+2.4)/etaStep;
-
+    
     // Down weight, then up weight
-    outWeights(0, ieta) = scaleWeight(weights[centralIdx-step10MeV], scaleFac);
-    outWeights(1, ieta) = scaleWeight(weights[centralIdx+step10MeV], scaleFac);
+    outWeights(0, ieta) = scaleWeight(weights[centralIdx-step10MeV], scaleFac)*nominal_weight;
+    outWeights(1, ieta) = scaleWeight(weights[centralIdx+step10MeV], scaleFac)*nominal_weight;
     return outWeights;
 }
 
