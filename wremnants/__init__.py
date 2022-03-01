c81c2c72c2d1c7eecf63d2c328cc559642657fce
Josh Bendavid <Josh.Bendavid@cern.ch>
2022-03-01 22:03:41 +0000

    add helpers for layer by layer corrections and placeholder uncertainties based on weights from tracking response, as well as truth-assisted layer by layer correction data for MC

--- wremnants/__init__.py
+++ wremnants/__init__.py
@@ -16,5 +16,6 @@ from .qcdScaleByHelicity_helper import makeQCDScaleByHelicityHelper
 from .pileup import make_pileup_helper
 from .syst_tools import scale_helicity_hist_to_variations
 from .theory_tools import axis_helicity, scale_tensor_axes, define_prefsr_vars, moments_to_angular_coeffs
+from .muon_calibration import make_muon_calibration_helpers, define_corrected_muons
 
 data_dir = f"{pathlib.Path(__file__).parent}/data/"
