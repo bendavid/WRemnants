451906d1211bdc360df7c94233cffc7f743d13fa
Marco Cipriani <marco.cipriani@cern.ch>
2022-05-18 09:11:53 +0200

    remove function that is no longer needed

--- wremnants/OutputTools.py
+++ wremnants/OutputTools.py
@@ -14,19 +14,3 @@ def fillTemplatedFile(templateFile, outFile, templateDict, append=False):
     filled = readFromTempate(templateFile, templateDict)
     with open(outFile, "w" if not append else "a") as outFile:
         outFile.write(result)
-
-
-def resetBinError(histo):
-    # TODO: move to a C++ helper function to be faster
-    if "TH1" in histo.ClassName():
-        for b in range(0,histo.GetNbinsX()+2):
-            histo.SetBinError(b, 0.0)
-    elif "TH2" in histo.ClassName():
-        for bx in range(0,histo.GetNbinsX()+2):
-            for by in range(0,histo.GetNbinsY()+2):
-                histo.SetBinError(bx, by, 0.0)
-    elif "TH3" in histo.ClassName():
-        for bx in range(0,histo.GetNbinsX()+2):
-            for by in range(0,histo.GetNbinsY()+2):
-                for bz in range(0,histo.GetNbinsZ()+2):
-                    histo.SetBinError(bx, by, bz, 0.0)
