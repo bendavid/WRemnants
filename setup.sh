6119eac89c4ff1de77b13acd673b612a18184329
Marco Cipriani <marco.cipriani@cern.ch>
2023-01-02 12:19:19 +0100

    fixes

--- setup.sh
+++ setup.sh
@@ -14,6 +14,8 @@ if [[ "$HOSTNAME" == *"lxplus8s10.cern.ch"* ]]; then
     COMBINE_STUDIES="/scratch/${USER}/CombineStudies/"
 #elif [[ "$HOSTNAME" == *"mit.edu"* ]]; then
 #    COMBINE_STUDIES="/data/submit/cms/store/${USER}/CombineStudies/" # FIXME: EDIT PATH
+elif [[ "$HOSTNAME" == *"cmsanalysis.pi.infn.it"* ]]; then
+    COMBINE_STUDIES="/scratchnvme/${USER}/CombineStudies/"
 else
     echo "Unknown hostname ${HOSTNAME}. Please update ${BASH_SOURCE}"
     exit 0
@@ -29,6 +31,18 @@ PLOTS="/eos/user/${USER:0:1}/${USER}/www/WMassAnalysis/"
 mkdir -pv ${PLOTS}
 echo "Created environment variable PLOTS=${PLOTS}"
 
-echo "Creating links to output folders"
-ln -sv ${PLOTS} ${WREM_BASE}/scripts/analysisTools/plots
-ln -sv ${COMBINE_STUDIES} ${WREM_BASE}/CombineStudies
+echo "Creating links to output folders (if not existing)"
+
+plotLink="${WREM_BASE}/scripts/analysisTools/plots"
+if [ -L ${plotLink} ] && [ -e ${plotLink} ]; then
+    echo "Existing link ${plotLink} --> ${PLOTS}"
+else
+    ln -sv ${PLOTS} ${plotLink}
+fi
+combineLink="${WREM_BASE}/CombineStudies"
+if [ -L ${combineLink} ] && [ -e ${combineLink} ]; then
+    echo "Existing link ${combineLink} --> ${COMBINE_STUDIES}"
+else
+    ln -sv ${COMBINE_STUDIES} ${combineLink}
+fi
+
