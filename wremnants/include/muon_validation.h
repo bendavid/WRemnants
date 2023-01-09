7b28245c9a161c1ffcc21615823d22f58920180b
Tianyu Justin Yang <tyjyang@mit.edu>
2023-01-09 01:22:44 +0100

    smearing weights in jpsi helper

--- wremnants/include/muon_validation.h
+++ wremnants/include/muon_validation.h
@@ -54,151 +54,135 @@ public:
             const double e_unc = params(1, i);
             const double M_unc = params(2, i);
             double k_crctd = 1.0 / jpsi_crctd_pt;
-            double k_shift = (A_unc - e_unc * k) * k + charge * M_unc;
-            double k_crctd_up = k_crctd + k_shift, k_crctd_down = k_crctd - k_shift;
-            res(i, 0) = (1.0 / k_crctd_up) - cvh_pt;
-            res(i, 1) = (1.0 / k_crctd_down) - cvh_pt;
+            double k_unc = (A_unc - e_unc * k) * k + charge * M_unc;
+            double k_crctd_up = k_crctd + k_unc, k_crctd_down = k_crctd - k_unc;
+            res(i, 0) = cvh_pt - (1.0 / k_crctd_down);
+            res(i, 1) = (1.0 / k_crctd_up) - cvh_pt;
         }
         return res;
     }
 
     // for smearing weights derived from qop
     out_tensor_t operator() (
-        float cvh_eta, float cvh_pt, int cvh_charge, float jpsi_crctd_pt,
-        double qop, double pt, double eta, double phi, int charge
+        double genQop, double genPhi, int genCharge, double genEta, double genPt, //for GEN params
+        double qop, double phi, int cvhCharge,
+        float cvhEta, float cvhPt, float jpsiCrctdPt, // for RECO params
+        const RVec<float> &cov // for sigma on the Gaussian
+        const auto &deltaPts // for the variations
+        bool abQop = false, bool fullParam = false
     ) {
-        const auto &params = get_tensor(cvh_eta);
-        double k = 1.0 / cvh_pt;
+        const double cvhLam = calculateLam(cvhEta);
+        const double cvhQopAbPt = calculateQop(cvhPt, cvhEta, cvhCharge);
+        const Eigen::Vector3d parms(
+            (abQop? qop : cvhQopAbPt),
+            (fullParam? cvhLam : 0),
+            (fullParam? phi : 0)
+        );
+    
+        const double genLam = calculateLam(genEta);
+        const double genQopAbPt = calculateQop(genPt, genEta, genCharge);
+        const Eigen::Vector3d genparms(
+            (abQop? genQop : genQopAbPt),
+            (fullParam? genLam : 0),
+            (fullParam? genPhi : 0)
+        );
+    
+        const Eigen::Vector3d deltaparms = parms - genparms;
+    
+        const Eigen::Map<const Eigen::Matrix<float, 3, 3, Eigen::RowMajor>> covMap(cov.data(), 3, 3);
+    
+        Eigen::Matrix<double, 3, 3> covd = covMap.cast<double>();
+    
+        if (fullParam) {
+            // fill in lower triangular part of the matrix, which is stored as zeros to save space
+            covd.triangularView<Eigen::Lower>() = covd.triangularView<Eigen::Upper>().transpose();
+        } else {
+            covd.row(0) << covd(0,0), 0, 0;
+            covd.row(1) << 0, 1, 0;
+            covd.row(2) << 0, 0, 1;
+        }
+    
+        const Eigen::Matrix<double, 3, 3> covinv = covd.inverse();
+        const double covdet = covd.determinant();
+    
+        const double lnp = -0.5*deltaparms.transpose()*covinv*deltaparms;
+    
+        // no need to initialize since all elements are explicit filled
         out_tensor_t res;
-        for (int i = 0; i < nUnc; i++) {
-            const double A_unc = params(0, i);
-            const double e_unc = params(1, i);
-            const double M_unc = params(2, i);
-            double k_crctd = 1.0 / jpsi_crctd_pt;
-            double k_shift = (A_unc - e_unc * k) * k + cvh_charge * M_unc;
-            double k_crctd_up = k_crctd + k_shift, k_crctd_down = k_crctd - k_shift;
-            res(i, 0) = (1.0 / k_crctd_up) - cvh_pt;
-            res(i, 1) = (1.0 / k_crctd_down) - cvh_pt;
+    
+        for (std::ptrdiff_t ivar = 0; ivar < nvars; ++ivar) {
+
+            deltaQopDown = calculteDeltaQop(cvhPt, deltaPts(ivar, 0), cvhEta, cvhCharge);
+            deltaQopup = calculteDeltaQop(cvhPt, deltaPts(ivar, 1), cvhEta, cvhCharge);
+    
+            for (std::ptrdiff_t idownup = 0; idownup < 2; ++idownup) {
+                Eigen::Vector3d parmvar = Eigen::Vector3d::Zero();   
+                const double dir = idownup == 0 ? -1. : 1.;
+                parmvar[0] =  idownup == 0 ? deltaQopDown : deltaQopUp;
+                const Eigen::Vector3d deltaparmsalt = deltaparms + dir*parmvar;
+                const Eigen::Matrix<double, 3, 3> covdalt = covd + dir*covvar;
+    
+                const Eigen::Matrix<double, 3, 3> covinvalt = covdalt.inverse();
+                const double covdetalt = covdalt.determinant();
+    
+                const double lnpalt = -0.5*deltaparmsalt.transpose()*covinvalt*deltaparmsalt;
+    
+                const double weight = std::sqrt(covdet/covdetalt)*std::exp(lnpalt - lnp);
+    
+                if (false) {
+                    if (std::isnan(weight) || std::isinf(weight) || std::fabs(weight) == 0.) {
+                        std::cout << "invalid weight: " << weight << std::endl;
+                        std::cout << "pt " << pt << std::endl;
+                        std::cout << "genPt " << genPt << std::endl;
+                        std::cout << "qop " << qop << std::endl;
+                        std::cout << "lam " << lam << std::endl;
+                        std::cout << "genlam " << genlam << std::endl;
+                        std::cout << "phi " << phi << std::endl;
+                        std::cout << "genlam " << genPhi << std::endl;
+                        std::cout << "covdet " << covdet << std::endl;
+                        std::cout << "covdetalt " << covdet << std::endl;
+                        std::cout << "lnp " << lnp << std::endl;
+                        std::cout << "lnpalt " << lnpalt << std::endl;
+                        std::cout << "covd\n" << covd << std::endl;
+                        std::cout << "covdalt\n" << covdalt << std::endl;
+                        std::cout << "covinv\n" << covinv << std::endl;
+                        std::cout << "covinvalt\n" << covinvalt << std::endl;
+                    }
+                }
+    
+            // protect against outliers
+            // if (weight > 0.9998 && weight < 1.0002) {cout << "smearing weight is " << weight << "covd is " << covd << std::endl;}
+                res(ivar, idownup) = std::min(weight, maxWeight_);
+            }
         }
         return res;
     }
 
 private:
-
-  out_tensor_t smearing_weight(
-    double qop, double pt, double eta, double phi, int charge, const RVec<float> &cov,
-    double genQop, double genPt, double genEta, double genPhi, int genCharge,
-    bool abQop = false, bool fullParam = false
-  ) const {
-
-    const double theta = 2.*std::atan(std::exp(-double(eta)));
-    const double lam = M_PI_2 - theta;
-    const double p = double(pt)/std::sin(theta);
-    const double Qop = double(charge)/p;
-    const Eigen::Vector3d parms(
-      (abQop? qop : Qop),
-      (fullParam? lam : 0),
-      (fullParam? phi : 0)
-    );
-
-    const double gentheta = 2.*std::atan(std::exp(-double(genEta)));
-    const double genlam = M_PI_2 - gentheta;
-    const double genp = double(genPt)/std::sin(gentheta);
-    const double genqop = double(genCharge)/genp;
-    const Eigen::Vector3d genparms(
-      (abQop? genQop : genqop),
-      (fullParam? genlam : 0),
-      (fullParam? genPhi : 0)
-    );
-
-    const Eigen::Vector3d deltaparms = parms - genparms;
-
-    const Eigen::Map<const Eigen::Matrix<float, 3, 3, Eigen::RowMajor>> covMap(cov.data(), 3, 3);
-
-    Eigen::Matrix<double, 3, 3> covd = covMap.cast<double>();
-
-    if (fullParam) {
-      // fill in lower triangular part of the matrix, which is stored as zeros to save space
-      covd.triangularView<Eigen::Lower>() = covd.triangularView<Eigen::Upper>().transpose();
-    } else {
-      covd.row(0) << covd(0,0), 0, 0;
-      covd.row(1) << 0, 1, 0;
-      covd.row(2) << 0, 0, 1;
-
-    }
-
-    const Eigen::Matrix<double, 3, 3> covinv = covd.inverse();
-    const double covdet = covd.determinant();
-
-    const double lnp = -0.5*deltaparms.transpose()*covinv*deltaparms;
-
-    // no need to initialize since all elements are explicit filled
-    out_tensor_t res;
-
-    const auto &varparms = hist_->at(hist_->template axis<0>().index(genEta)).data();
-
-    for (std::ptrdiff_t ivar = 0; ivar < nvars; ++ivar) {
-      Eigen::Vector3d parmvar = Eigen::Vector3d::Zero();
-      Eigen::Matrix<double, 3, 3> covvar = Eigen::Matrix<double, 3, 3>::Zero();
-
-      const double A = varparms(ivar, 0);
-      const double e = varparms(ivar, 1);
-      const double M = varparms(ivar, 2);
-      const double sig = varparms(ivar, 3);
-
-      // scale
-      parmvar[0] += (abQop? A*genQop : A*genqop);
-      parmvar[0] += (abQop? -e*genQop/genPt : -e*genqop/genPt);
-      parmvar[0] += (abQop? genCharge*M*genPt*genQop : genCharge*M*genPt*genqop);
-
-      // resolution
-      covvar += sig*covd;
-
-      for (std::ptrdiff_t idownup = 0; idownup < 2; ++idownup) {
-
-        const double dir = idownup == 0 ? -1. : 1.;
-
-        const Eigen::Vector3d deltaparmsalt = deltaparms + dir*parmvar;
-        const Eigen::Matrix<double, 3, 3> covdalt = covd + dir*covvar;
-
-        const Eigen::Matrix<double, 3, 3> covinvalt = covdalt.inverse();
-        const double covdetalt = covdalt.determinant();
-
-        const double lnpalt = -0.5*deltaparmsalt.transpose()*covinvalt*deltaparmsalt;
-
-        const double weight = std::sqrt(covdet/covdetalt)*std::exp(lnpalt - lnp);
-
-        if (false) {
-          if (std::isnan(weight) || std::isinf(weight) || std::fabs(weight) == 0.) {
-            std::cout << "invalid weight: " << weight << std::endl;
-            std::cout << "pt " << pt << std::endl;
-            std::cout << "genPt " << genPt << std::endl;
-            std::cout << "qop " << qop << std::endl;
-            std::cout << "lam " << lam << std::endl;
-            std::cout << "genlam " << genlam << std::endl;
-            std::cout << "phi " << phi << std::endl;
-            std::cout << "genlam " << genPhi << std::endl;
-            std::cout << "covdet " << covdet << std::endl;
-            std::cout << "covdetalt " << covdet << std::endl;
-            std::cout << "lnp " << lnp << std::endl;
-            std::cout << "lnpalt " << lnpalt << std::endl;
-            std::cout << "covd\n" << covd << std::endl;
-            std::cout << "covdalt\n" << covdalt << std::endl;
-            std::cout << "covinv\n" << covinv << std::endl;
-            std::cout << "covinvalt\n" << covinvalt << std::endl;
-          }
-        }
-
-        // protect against outliers
-        // if (weight > 0.9998 && weight < 1.0002) {cout << "smearing weight is " << weight << "covd is " << covd << std::endl;}
-        res(ivar, idownup) = std::min(weight, maxWeight_);
-      }
-    }
-    
-    return res;
-  }
     std::shared_ptr<const T> correctionHist_;
-
 };
 
+    double calculateTheta(float eta) {
+        return 2.*std::atan(std::exp(-double(eta)));
+    }
+
+    double calculateLam(float eta) {
+        theta = calculateTheta(eta);
+        return M_PI_2 - theta;
+    }
+
+    double calculateQop(float pt, float eta, int charge) {
+        double theta = calculateTheta(eta);
+        return (charge * std::sin(theta) / double(pt));
+    }
+
+    double calculatePt(double qop, float eta, int charge) {
+        double theta = calculateTheta(eta);
+        return (charge * std::sin(theta) / qop);
+    }
+
+    double calculateDeltaQop(float pt, float deltaPt, float eta, int charge) {
+        deltaK = -deltaPt / (pt * (pt + deltaPt));
+        return charge * std::sin(calculateTheta(eta)) * deltaK;
+    }
 }
