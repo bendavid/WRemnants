0de1853afe1c948cb600cab0aba2a79a91a1a4ec
Tianyu Justin Yang <tyjyang@mit.edu>
2023-01-26 12:27:55 +0100

    central value helper for muon vectors

--- wremnants/include/muon_validation.h
+++ wremnants/include/muon_validation.h
@@ -1,5 +1,6 @@
 #include <boost/histogram.hpp>
 #include <stdlib.h>
+#include <defines.h>
 
 namespace wrem {
 
@@ -22,20 +23,15 @@ namespace wrem {
         return (charge * std::sin(theta) / qop);
     }
 
-    double calculateDeltaQop(float pt, float deltaPt, float eta, int charge) {
-        double deltaK = -deltaPt / (pt * (pt + deltaPt));
-        return charge * std::sin(calculateTheta(eta)) * deltaK;
-    }
-
+// jpsi correction central value for one muon
 template <typename T>
-class JpsiCorrectionsHelper {
+class JpsiCorrectionsHelperSingle {
 
 public:
-
     using hist_t = T;
     using tensor_t = typename T::storage_type::value_type::tensor_t;
     static constexpr auto sizes = narf::tensor_traits<tensor_t>::sizes;
-    static constexpr auto nUnc = sizes[sizes.size() - 1]; // 1 for cnetral value
+    static constexpr auto nUnc = sizes[sizes.size() - 1]; // 1 for central value
     using out_tensor_t = Eigen::TensorFixedSize<double, Eigen::Sizes<nUnc, 2>>;
 
     JpsiCorrectionsHelper(T&& corrections) :
@@ -54,68 +50,91 @@ public:
     }
     
     // for central value of pt
-    double operator() (float cvh_eta, float cvh_pt, int charge) {
-        const auto &params = get_tensor(cvh_eta);
+    double operator() (float cvhEta, float cvhPt, int charge) {
+        const auto &params = get_tensor(cvhEta);
         const double A = params(0, 0);
         const double e = params(1, 0);
         const double M = params(2, 0);
-        double k = 1.0 / cvh_pt;
+        double k = 1.0 / cvhPt;
         double magnetic = 1.0 + A;
         double material = -1.0 * e * k;
         double alignment = charge * M;
-        double k_crctd = (magnetic + material) * k + alignment;
-        return (1.0 / k_crctd);
-    }
-    /*
-    // for uncertainties on pt
-    out_tensor_t operator() (
-        float cvh_eta, float cvh_pt, int charge, float jpsi_crctd_pt
-    ) {
-        const auto &params = get_tensor(cvh_eta);
-        double k = 1.0 / cvh_pt;
-        out_tensor_t res;
-        for (int i = 0; i < nUnc; i++) {
-            const double A_unc = params(0, i);
-            const double e_unc = params(1, i);
-            const double M_unc = params(2, i);
-            double k_crctd = 1.0 / jpsi_crctd_pt;
-            double k_unc = (A_unc - e_unc * k) * k + charge * M_unc;
-            double k_crctd_up = k_crctd + k_unc, k_crctd_down = k_crctd - k_unc;
-            res(i, 0) = cvh_pt - (1.0 / k_crctd_down);
-            res(i, 1) = (1.0 / k_crctd_up) - cvh_pt;
-        }
-        return res;
+        double kCrctd = (magnetic + material) * k + alignment;
+        return (1.0 / kCrctd);
     }
 
-    // for smearing weights derived from qop
-    out_tensor_t operator() (
-        double genQop, double genPhi, int genCharge, double genEta, double genPt, //for GEN params
-        double qop, double phi, int cvhCharge,
-        float cvhEta, float cvhPt, float jpsiCrctdPt, // for RECO params
+private:
+    std::shared_ptr<const T> correctionHist_;
+
+};
+
+// jpsi corrections central value for multiple muons
+class JpsiCorrectionsHelper : public JpsiCorrectionsHelperSingle<> {
+
+public:
+    JpsiCorrectionsHelper(T&& corrections) : JpsiCorrectionsHelperSingle(corrections) {}
+
+    Vec_d operator() (const Vec_f &cvhEtas, const Vec_f &cvhPts, const Vec_i &charges) {
+        Vec_d res;
+        res.reserve(cvhEtas.size());
+        for (unsigned int i = 0; i < cvhEtas.size(); ++i) {
+            res.emplace_back(
+                JpsiCorrectionsHelperSingle::operator()(cvhEtas[i], cvhPts[i], charges[i])
+            )
+        }
+    }
+};
+
+
+template <typename T>
+class JpsiCorrectionsUncHelper {
+
+public:
+    using hist_t = T;
+    using tensor_t = typename T::storage_type::value_type::tensor_t;
+    static constexpr auto sizes = narf::tensor_traits<tensor_t>::sizes;
+    static constexpr auto nUnc = sizes[sizes.size() - 1]; // 1 for central value
+    using out_tensor_t = Eigen::TensorFixedSize<double, Eigen::Sizes<nUnc, 2>>;
+
+    JpsiCorrectionsUncHelper(T&& corrections) :
+        correctionHist_(std::make_shared<const T>(std::move(corrections))) {}
+
+    // helper for bin lookup which implements the compile-time loop over axes
+    template<typename... Xs, std::size_t... Idxs>
+    const tensor_t &get_tensor_impl(std::index_sequence<Idxs...>, const Xs&... xs) {
+        return correctionHist_->at(correctionHist_->template axis<Idxs>().index(xs)...).data();
+    }
+
+    // variadic templated bin lookup
+    template<typename... Xs>
+    const tensor_t &get_tensor(const Xs&... xs) {
+        return get_tensor_impl(std::index_sequence_for<Xs...>{}, xs...);
+    }
+
+private:
+    // for smearing weights derived from propagating uncs on A, e, M to uncs on qop
+    out_tensor_t smwearing_weight() (
+        double genQop, double genPhi, int genCharge, double genEta, double genPt,
+        double recoQop, double recoPhi, int recoCharge, float recoEta, float recoPt,
         const RVec<float> &cov, // for sigma on the Gaussian
-        out_tensor_t &deltaPts, // for the variations
         bool abQop = false, bool fullParam = false
     ) {
-        const double cvhLam = calculateLam(cvhEta);
-        const double cvhQopAbPt = calculateQop(cvhPt, cvhEta, cvhCharge);
         const Eigen::Vector3d parms(
-            (abQop? qop : cvhQopAbPt),
-            (fullParam? cvhLam : 0),
-            (fullParam? phi : 0)
-        );
-    
-        const double genLam = calculateLam(genEta);
-        const double genQopAbPt = calculateQop(genPt, genEta, genCharge);
+            (abQop? recoQop : calculateQop(recoPt, recoEta, recoCharge)),
+            (fullParam? calculateLam(recoEta) : 0),
+            (fullParam? recoPhi: 0)
+        ); // (qop, lam, phi)
+
         const Eigen::Vector3d genparms(
-            (abQop? genQop : genQopAbPt),
-            (fullParam? genLam : 0),
-            (fullParam? genPhi : 0)
+            (abQop? genQop : calculateQop(genPt, genEta, genCharge)),
+            (fullParam? calculateLam(genEta) : 0),
+            (fullParam? genPhi: 0)
         );
-    
+
         const Eigen::Vector3d deltaparms = parms - genparms;
-    
+
         const Eigen::Map<const Eigen::Matrix<float, 3, 3, Eigen::RowMajor>> covMap(cov.data(), 3, 3);
-    
+
         Eigen::Matrix<double, 3, 3> covd = covMap.cast<double>();
     
         if (fullParam) {
@@ -126,24 +145,28 @@ public:
             covd.row(1) << 0, 1, 0;
             covd.row(2) << 0, 0, 1;
         }
-    
+
         const Eigen::Matrix<double, 3, 3> covinv = covd.inverse();
         const double covdet = covd.determinant();
     
         const double lnp = -0.5*deltaparms.transpose()*covinv*deltaparms;
-    
-        // no need to initialize since all elements are explicit filled
+
+        const auto &params = get_tensor(recoEta);
+
+        // no need to initialize since all elements will be explicitly filled
         out_tensor_t res;
     
         for (std::ptrdiff_t ivar = 0; ivar < nUnc; ++ivar) {
+            const double AUnc = params(0, ivar);
+            const double eUnc = params(1, ivar);
+            const double MUnc = params(2, ivar);
+            double recoK = 1.0 /recoPt;
+            double recoKUnc = (AUnc - eUnc * recoK) * recoK + charge * MUnc;
+            Eigen::Vector3d parmvar = Eigen::Vector3d::Zero();   
+            parmvar[0] = charge * std::sin(calculateTheta(cvhEta)) * recoKUnc;
 
-            double deltaQopDown = calculteDeltaQop(cvhPt, deltaPts(ivar, 0), cvhEta, cvhCharge);
-            double deltaQopUp = calculteDeltaQop(cvhPt, deltaPts(ivar, 1), cvhEta, cvhCharge);
-    
             for (std::ptrdiff_t idownup = 0; idownup < 2; ++idownup) {
-                Eigen::Vector3d parmvar = Eigen::Vector3d::Zero();   
                 const double dir = idownup == 0 ? -1. : 1.;
-                parmvar[0] =  idownup == 0 ? deltaQopDown : deltaQopUp;
                 const Eigen::Vector3d deltaparmsalt = deltaparms + dir*parmvar;
                 const Eigen::Matrix<double, 3, 3> covdalt = covd; //+ dir*covvar;
     
@@ -161,9 +184,9 @@ public:
         }
         return res;
     }
-    */
-private:
-    std::shared_ptr<const T> correctionHist_;
+
+    std::shared_ptr<const T> correctionUncHist_;
+
 };
 
 }
