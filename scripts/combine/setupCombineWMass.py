83acc4450cc3254fafbd9df336b86b9c335959ad
Markus Seidel <markus.seidel@cern.ch>
2023-02-10 16:46:29 +0100

    EW nuisance parameter

--- scripts/combine/setupCombineWMass.py
+++ scripts/combine/setupCombineWMass.py
@@ -18,6 +18,7 @@ def make_parser(parser=None):
         parser = common.common_parser_combine()
     parser.add_argument("--fitvar", help="Variable to fit", default="eta_pt", choices=sel.hist_map.keys())
     parser.add_argument("--noEfficiencyUnc", action='store_true', help="Skip efficiency uncertainty (useful for tests, because it's slow). Equivalent to --excludeNuisances '.*effSystTnP|.*effStatTnP' ")
+    parser.add_argument("--ewUnc", action='store_true', help="Include EW uncertainty")
     parser.add_argument("-p", "--pseudoData", type=str, help="Hist to use as pseudodata")
     parser.add_argument("--pseudoDataIdx", type=int, default=0, help="Variation index to use as pseudodata")
     parser.add_argument("--pseudodata-file", type=str, help="Input file for pseudodata (if it should be read from a different file", default=None)
@@ -169,6 +170,17 @@ def main(args):
         scale=0.75, # TODO: this depends on the set, should be provided in theory_tools.py
         passToFakes=passSystToFakes,
     )
+
+    if args.ewUnc:
+        cardTool.addSystematic(f"horacenloewCorr_unc", 
+            processes=single_v_samples,
+            mirror=True,
+            group="theory_ew",
+            systAxes=["systIdx"],
+            labelsByAxis=["horacenloewCorr_unc"],
+            skipEntries=[(0, -1), (2, -1)],
+            passToFakes=passSystToFakes,
+        )
 
     if not args.noEfficiencyUnc:
         chargeDependentSteps = common.muonEfficiency_chargeDependentSteps
