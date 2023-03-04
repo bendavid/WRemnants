88317a660a9b7087baa1d9cd5b471adc990f7f92
dbruschi <62343966+dbruschi@users.noreply.github.com>
2023-03-04 01:43:58 +0100

    Reimplementation of production of histograms for smoothing
    In a way which should take way less time and also requires less space (I apologize for not thinking about this earlier)

--- scripts/histmakers/3dsmoothing/makeefficiencies.py
+++ scripts/histmakers/3dsmoothing/makeefficiencies.py
@@ -8,19 +8,17 @@ import ROOT
 
 #FIRST STEP IN EFFICIENCY CREATION
 
-name = "rnd"
-
-directory = "directory_name"
+name = "rnf"
 
 histlist = []
 
-histlist.append(f"{directory_name}/mw_with_mu_eta_pt_scetlibCorr_idip{name}.hdf5")
-histlist.append(f"{directory_name}/mw_with_mu_eta_pt_scetlibCorr_triggerMC{name}.hdf5")
-histlist.append(f"{directory_name}/mw_with_mu_eta_pt_scetlibCorr_trigger{name}.hdf5")
-histlist.append(f"{directory_name}/mw_with_mu_eta_pt_scetlibCorr_trigger{name}error.hdf5")
-histlist.append(f"{directory_name}/mw_with_mu_eta_pt_scetlibCorr_isoMC{name}.hdf5")
-histlist.append(f"{directory_name}/mw_with_mu_eta_pt_scetlibCorr_iso{name}.hdf5")
-histlist.append(f"{directory_name}/mw_with_mu_eta_pt_scetlibCorr_iso{name}error.hdf5")
+directory = "directory_name"
+
+histlist.append(f"{directory}/mw_with_mu_eta_pt_scetlibCorr_idip{name}.hdf5")
+histlist.append(f"{directory}/mw_with_mu_eta_pt_scetlibCorr_trigger{name}.hdf5")
+histlist.append(f"{directory}/mw_with_mu_eta_pt_scetlibCorr_iso{name}.hdf5")
+
+listoflists = [["nominal"],["nominal_smoothMC","nominal","nominal_smoothErr"],["nominal_smoothMC","nominal","nominal_smoothErr"]] 
 
 namelist = []
 namelist.append("IDIP")
@@ -32,30 +30,33 @@ namelist.append("Iso")
 namelist.append("IsoError")
 
 histos = []
-    
+
+counter = 0
 for idx,filename in enumerate(histlist) :
     h5file = h5py.File(filename, "r")
     results = narf.ioutils.pickle_load_h5py(h5file["results"])
     plus = results["WplusmunuPostVFP"]["output"]
     minus = results["WminusmunuPostVFP"]["output"]
-    plus = plus["nominal"].get()
-    minus = minus["nominal"].get()
-    if "error" in filename:
-        plus = plus[:,:,1:2,:,:,:]
-        plus = plus[:,:,sum,sum,sum,:]
-        minus = minus[:,:,0:1,:,:,:]
-        minus = minus[:,:,sum,sum,sum,:]
-    else :
-        plus = plus[:,:,1:2,:,:]
-        plus = plus[:,:,sum,sum,sum]
-        minus = minus[:,:,0:1,:,:]
-        minus = minus[:,:,sum,sum,sum]
-    plus=narf.hist_to_root(plus)
-    plus.SetName(f"{namelist[idx]}Plus")
-    minus=narf.hist_to_root(minus)
-    minus.SetName(f"{namelist[idx]}Minus")
-    histos.append(plus)
-    histos.append(minus)
+    for i in range(0,len(listoflists[idx])):
+        plus = plus[listoflists[idx][i]].get()
+        minus = minus[listoflists[idx][i]].get()
+        if "Error" in namelist[counter]:
+            plus = plus[:,:,1:2,:,:,:]
+            plus = plus[:,:,sum,sum,sum,:]
+            minus = minus[:,:,0:1,:,:,:]
+            minus = minus[:,:,sum,sum,sum,:]
+        else :
+            plus = plus[:,:,1:2,:,:]
+            plus = plus[:,:,sum,sum,sum]
+            minus = minus[:,:,0:1,:,:]
+            minus = minus[:,:,sum,sum,sum]
+        plus=narf.hist_to_root(plus)
+        plus.SetName(f"{namelist[counter]}Plus")
+        minus=narf.hist_to_root(minus)
+        minus.SetName(f"{namelist[counter]}Minus")
+        histos.append(plus)
+        histos.append(minus)
+        counter = counter + 1
 
 output_file = ROOT.TFile(f"makeefficiencies{name}.root","RECREATE")
 output_file.cd()
