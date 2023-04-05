ef1c65f493c157c62d8dde3883b3de31c7dfb30c
David Walter <david.walter@.cern.ch>
2023-04-05 15:05:19 +0200

    Information of datagroups.groups is now stored in separate datagroup class objects instead of having a large complicated dict

--- wremnants/datasets/datagroups2016.py
+++ wremnants/datasets/datagroups2016.py
@@ -1,6 +1,7 @@
 from utilities import boostHistHelpers as hh, logging
 from wremnants import histselections as sel
 from wremnants.datasets.datagroups import Datagroups
+from wremnants.datasets.datagroup import Datagroup
 from wremnants.datasets import datasets2016
 
 logger = logging.child_logger(__name__)
@@ -18,88 +19,85 @@ class Datagroups2016(Datagroups):
             sigOp = None
             fakeOp = None
 
-        self.groups =  {
-            "Data" : dict(
-                members = self.getSafeListFromDataset(["dataPostVFP"]),
-                color = "black",
-                label = "Data",
-                selectOp = sigOp,
-            ),
-            "Zmumu" : dict(
-                members = self.getSafeListFromDataset(["ZmumuPostVFP"]),
-                #label = r"Z$\to\mu\mu$ (N$^{3}LL+NNLO)$",
-                label = r"Z$\to\mu\mu$",
-                color = "lightblue",
-                selectOp = sigOp,
-            ),   
-            "Ztautau" : dict(
-                members = self.getSafeListFromDataset(["ZtautauPostVFP"]),
-                label = r"Z$\to\tau\tau$",
-                color = "darkblue",
-                selectOp = sigOp,
-            ),            
-        }
+        self.addGroup("Data",
+            members = self.getSafeListFromDataset(["dataPostVFP"]),
+            color = "black",
+            label = "Data",
+            selectOp = sigOp,
+        )
+        self.addGroup("Zmumu",
+            members = self.getSafeListFromDataset(["ZmumuPostVFP"]),
+            #label = r"Z$\to\mu\mu$ (N$^{3}LL+NNLO)$",
+            label = r"Z$\to\mu\mu$",
+            color = "lightblue",
+            selectOp = sigOp,
+        ) 
+        self.addGroup("Ztautau",
+            members = self.getSafeListFromDataset(["ZtautauPostVFP"]),
+            label = r"Z$\to\tau\tau$",
+            color = "darkblue",
+            selectOp = sigOp,
+        )
+
         if pseudodata_pdfset and combine:
-            self.groups[f"pdf{pseudodata_pdfset.upper()}_sum"] = dict(
+            self.addGroup(f"pdf{pseudodata_pdfset.upper()}_sum",
                 label = f"pdf{pseudodata_pdfset.upper()}",
                 color = "dimgray"
             )
         if self.wmass:
-            self.groups.update({
-                "Wmunu" : dict(
-                    members = self.getSafeListFromDataset(["WminusmunuPostVFP", "WplusmunuPostVFP"]),
-                    label = r"W$^{\pm}\to\mu\nu$",
-                    color = "darkred",
-                    selectOp = sigOp,
-                ),
-                }
+            self.addGroup("Wmunu",
+                members = self.getSafeListFromDataset(["WminusmunuPostVFP", "WplusmunuPostVFP"]),
+                label = r"W$^{\pm}\to\mu\nu$",
+                color = "darkred",
+                selectOp = sigOp,
             )
-            # Reorder
-            for k in ["Zmumu", "Ztautau"]:
-                self.groups[k] = self.groups.pop(k)
-            self.groups.update({
-                "Wtaunu" : dict(
-                    members = self.getSafeListFromDataset(["WminustaunuPostVFP", "WplustaunuPostVFP"]),
-                    label = r"W$^{\pm}\to\tau\nu$",
-                    color = "orange",
-                    selectOp = sigOp,
-                ),
-                "Top" : dict(
-                    members = list(filter(lambda y: y.group == "Top", self.datasets.values())),
-                    label = "Top",
-                    color = "green",
-                    selectOp = sigOp,
-                ), 
-                "Diboson" : dict(
-                    members = list(filter(lambda y: y.group == "Diboson", self.datasets.values())),
-                    label = "Diboson",
-                    color = "pink",
-                    selectOp = sigOp,
-                ), 
-                "QCD" : dict(
-                    members = list(filter(lambda y: y.group == "QCD", self.datasets.values())),
-                    label = "QCD MC",
-                    color = "grey",
-                    selectOp = sigOp,
-                ), 
+            self.addGroup("Wtaunu",
+                members = self.getSafeListFromDataset(["WminustaunuPostVFP", "WplustaunuPostVFP"]),
+                label = r"W$^{\pm}\to\tau\nu$",
+                color = "orange",
+                selectOp = sigOp,
+            )
+            self.addGroup("Top",
+                members = list(filter(lambda y: y.group == "Top", self.datasets.values())),
+                label = "Top",
+                color = "green",
+                selectOp = sigOp,
+            )
+            self.addGroup("Diboson",
+                members = list(filter(lambda y: y.group == "Diboson", self.datasets.values())),
+                label = "Diboson",
+                color = "pink",
+                selectOp = sigOp,
+            )
+            self.addGroup("QCD",
+                members = list(filter(lambda y: y.group == "QCD", self.datasets.values())),
+                label = "QCD MC",
+                color = "grey",
+                selectOp = sigOp,
+            )
            
-            })
         else:
-            self.groups["Other"] = dict(
+            self.addGroup("Other",
                 members = [x for x in self.datasets.values() if not x.is_data and x.name not in ["ZmumuPostVFP", "ZtautauPostVFP"] and x.group != "QCD"],
                 label = "Other",
                 color = "grey",
             )
-        
+
         self.filterGroups(filterGroups)
         self.excludeGroups(excludeGroups)
-
+        
         if self.wmass:
             # add all processes to the fake contributions after filtered and excluded groups
-            self.groups["Fake"] = dict(
-                members = [member for sublist in [self.groups[x]["members"] for x in self.groups if x != "QCD"] for member in sublist],
+            self.addGroup("Fake",
+                members = [member for sublist in [v.members for k, v in self.groups.items() if k != "QCD"] for member in sublist],
                 scale = lambda x: 1. if x.is_data else -1,
                 label = "Nonprompt",
                 color = "grey",
                 selectOp = fakeOp,
             )
+
+        if self.wmass:
+            self.gen_axes = ["etaGen", "ptGen"]
+        elif self.wlike:
+            self.gen_axes = ["qGen", "etaGen", "ptGen"]
+
