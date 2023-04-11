b6909ec725b395199ab596d5f39d35469900dc22
David Walter <david.walter@.cern.ch>
2023-04-11 22:26:52 +0200

    Attempt to fix CI

--- scripts/ci/run_combine.sh
+++ scripts/ci/run_combine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ $# -lt 3 ]]; then
-	echo "Requires at least three arguments: run_combine.sh <combinetf_dir> <mode> <combine_cards>"
+if [[ $# -lt 4 ]]; then
+	echo "Requires at least four arguments: run_combine.sh <combinetf_dir> <mode> <working_dir> <combine_cards>"
 	exit 1
 fi
 
@@ -10,9 +10,9 @@ combinetf_dir=$1
 mode=$2
 
 cards=()
-for card in ${@:3}; do
+for card in ${@:4}; do
 	echo $card
-	cards+=( `basename $card` )
+	cards+=( $card )
 done
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
@@ -21,14 +21,10 @@ eval `scram runtime -sh`
 popd
 
 working_dir=`dirname $3`
-card_name=${cards[0]}
-
 pushd $working_dir
 
-if [[ $# -gt 2 ]]; then
-	card_name=${working_dir}.txt
-	combineCards.py $cards > $card_name
-fi
+card_name=${working_dir}.txt
+combineCards.py $cards > $card_name
 
 outfile=${card_name/txt/hdf5}
 
@@ -37,5 +33,5 @@ if [ "$mode" == "mass" ]; then
 	combinetf.py --doImpacts --binByBinStat -t -1 "$outfile"
 elif [ "$mode" == "unfold" ]; then
 	text2hdf5.py --X-allow-no-background --maskedChan=xnorm "$card_name"
-	combinetf.py --doImpacts --binByBinStat -t 1 --saveHists --computeHistErrors --correlateXsecStat "$outfile"
+	combinetf.py --doImpacts --binByBinStat -t -1 --saveHists --computeHistErrors --correlateXsecStat "$outfile"
 fi
