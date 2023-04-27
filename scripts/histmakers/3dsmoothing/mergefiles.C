91bbc446606c8e9827a9bdee093d9429cd139587
Davide Bruschini <davide.bruschini@cern.ch>
2023-04-27 11:25:00 +0200

    intermediate commit

--- scripts/histmakers/3dsmoothing/mergefiles.C
+++ scripts/histmakers/3dsmoothing/mergefiles.C
@@ -1,7 +1,7 @@
 void mergefiles() {
-	TFile *file=new TFile("/gpfs/ddn/cms/user/bruschin/Newtest/smoothLeptonScaleFactorswrongwayofdoingthings/GtoH/allSmooth_GtoH.root");
-	TFile *file2=new TFile("/gpfs/ddn/cms/user/bruschin/Newtest/smoothLeptonScaleFactors3dcrosscheck/GtoH/allSmooth_GtoH.root");
-	TFile *file3=new TFile("/gpfs/ddn/cms/user/bruschin/Newtest/smoothLeptonScaleFactorswrongwayofdoingthings/GtoH/allSmooth_GtoH3.root","RECREATE");
+	TFile *file=new TFile("/gpfs/ddn/cms/user/bruschin/Newtest/smoothLeptonScaleFactorswrongwayofdoingthingspol6/GtoH/allSmooth_GtoH.root");
+	TFile *file2=new TFile("/gpfs/ddn/cms/user/bruschin/Newtest/smoothLeptonScaleFactorsredofull/GtoH/allSmooth_GtoH.root");
+	TFile *file3=new TFile("/gpfs/ddn/cms/user/bruschin/Newtest/smoothLeptonScaleFactorswrongwayofdoingthingspol6/GtoH/allSmooth_GtoH2.root","RECREATE");
 	file3->cd();
 	for (auto&& keyAsObj : *(file2->GetListOfKeys())){
 		auto key = (TKey*) keyAsObj;
