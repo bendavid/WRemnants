33f715ac1b23444aa712fb75e707fc3ce9280d72
David Walter <david.walter@.cern.ch>
2023-05-12 22:19:00 +0200

    Revert "Remove overflow and underflow bins in main axes"
    This reverts commit 2101551f0ff24ecda7c07b7f8357edd5114ba15f.

--- scripts/histmakers/mz_wlike_with_mu_eta_pt.py
+++ scripts/histmakers/mz_wlike_with_mu_eta_pt.py
@@ -37,8 +37,8 @@ template_maxpt = args.pt[2]
 logger.info(f"Pt binning: {template_npt} bins from {template_minpt} to {template_maxpt}")
 
 # standard regular axes
-axis_eta = hist.axis.Regular(template_neta, template_mineta, template_maxeta, name = "eta", overflow=args.includeFlow, underflow=args.includeFlow)
-axis_pt = hist.axis.Regular(template_npt, template_minpt, template_maxpt, name = "pt", overflow=args.includeFlow, underflow=args.includeFlow)
+axis_eta = hist.axis.Regular(template_neta, template_mineta, template_maxeta, name = "eta")
+axis_pt = hist.axis.Regular(template_npt, template_minpt, template_maxpt, name = "pt")
 
 # categorical axes in python bindings always have an overflow bin, so use a regular
 # axis for the charge
