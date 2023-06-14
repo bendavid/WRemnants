82be14c5ce5542281a4da715792c6dbbe0a1e1a8
Kenneth Long <kdlong@hep.wisc.edu>
2023-06-14 12:10:47 +0200

    Update CI to be consistent with impact plot changes

--- scripts/ci/show_impacts.sh
+++ scripts/ci/show_impacts.sh
@@ -7,8 +7,12 @@ fi
 
 . ./setup.sh
 python3 scripts/combine/printImpacts.py -f $1 
+<<<<<<< HEAD
 # TODO: Add option to do this in the same call to the function
 python3 scripts/combine/pullsAndImpacts.py --oneSidedImpacts -g -f $1 output --outFolder $2 -o ${3/.html/_grouped.html} --otherExtensions pdf png --eoscp
 python3 scripts/combine/pullsAndImpacts.py --oneSidedImpacts -f $1 output --outFolder $2 -o $3 --otherExtensions pdf png -n 50 --eoscp
+=======
+python3 scripts/combine/pullsAndImpacts.py --oneSidedImpacts -f $1 output --outFolder $2 -o $3 --otherExtensions pdf png -n 50
+>>>>>>> 71c2c47 (Update CI to be consistent with impact plot changes)
 # Don't limit the number for the html plot
 python3 scripts/combine/pullsAndImpacts.py --oneSidedImpacts -f $1 output --outFolder $2 -o $3 --eoscp
