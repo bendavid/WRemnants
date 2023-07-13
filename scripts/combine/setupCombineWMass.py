064b83f658cb02c129232bd5de9c276c184377f9
Marco Cipriani <marco.cipriani@cern.ch>
2023-07-13 10:59:26 +0200

    option to choose absolute paths for files in datacard

--- scripts/combine/setupCombineWMass.py
+++ scripts/combine/setupCombineWMass.py
@@ -38,7 +38,8 @@ def make_parser(parser=None):
     parser.add_argument("--correlatedNonClosureNuisances", action='store_true', help="get systematics from histograms for the Z non-closure nuisances without decorrelation in eta and pt")
     parser.add_argument("--sepImpactForNC", action="store_true", help="use a dedicated impact gropu for non closure nuisances, instead of putting them in muonScale")
     parser.add_argument("--genModel", action="store_true", help="Produce datacard with the xnorm as model (binned according to axes defined in --fitvar)")
-
+    # TODO: move next option in common.py? 
+    parser.add_argument("--absolutePathInCard", action="store_true", help="In the datacard, set Absolute path for the root file where shapes are stored")
     return parser
 
 def main(args,xnorm=False):   
@@ -120,6 +121,8 @@ def main(args,xnorm=False):
     logger.debug(f"Making datacards with these processes: {cardTool.getProcesses()}")
     cardTool.setNominalTemplate(f"{templateDir}/main.txt")
     cardTool.setProjectionAxes(args.fitvar.split("-"))
+    if args.absolutePathInCard:
+        cardTool.setAbsolutePathShapeInCard()
     if args.sumChannels or xnorm or name in ["ZMassDilepton"]:
         cardTool.setChannels(["inclusive"])
         cardTool.setWriteByCharge(False)
@@ -195,10 +198,10 @@ def main(args,xnorm=False):
     )
     
     if args.doStatOnly:
-        # print a card with only mass weights and a dummy syst
-        cardTool.addLnNSystematic("dummy", processes=["Top", "Diboson"] if wmass else ["Other"], size=1.001, group="dummy")
+        # print a card with only mass weights, no longer need a dummy syst since combinetf is fixed now
+        #cardTool.addLnNSystematic("dummy", processes=["Top", "Diboson"] if wmass else ["Other"], size=1.001, group="dummy")
         cardTool.writeOutput(args=args, xnorm=xnorm)
-        logger.info("Using option --doStatOnly: the card was created with only mass weights and a dummy LnN syst on all processes")
+        logger.info("Using option --doStatOnly: the card was created with only mass nuisance parameter")
         return
 
     if not xnorm:
