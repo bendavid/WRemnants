08ddb88936b6ec2fd03f7704de594b396e717c7a
Josh Bendavid <Josh.Bendavid@cern.ch>
2023-09-20 08:53:49 -0400

    add resolution uncertainties for wlike and dilepton

--- scripts/histmakers/mz_dilepton.py
+++ scripts/histmakers/mz_dilepton.py
@@ -317,6 +317,8 @@ def build_graph(df, dataset):
                 )
                 results.append(muonScaleSyst_responseWeights)
 
+                df = muon_calibration.add_resolution_uncertainty(df, axes, results, cols, smearing_uncertainty_helper, reco_sel_GF)
+
                 # add the ad-hoc Z non-closure nuisances from the jpsi massfit to muon scale unc
                 df = df.DefinePerSample("AFlag", "0x01")
                 df = df.Define(
