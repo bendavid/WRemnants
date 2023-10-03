b2b0efecfe8a4ffa5cca0cf90aeb886c50bac658
David <david.walter@cern.ch>
2023-10-03 09:50:41 +0200

    Update nuisance grouping for muon calibration

--- scripts/combine/setupCombine.py
+++ scripts/combine/setupCombine.py
@@ -55,8 +55,6 @@ def make_parser(parser=None):
     parser.add_argument("--muonScaleVariation", choices=["smearingWeights", "massWeights", "manualShift"], default="smearingWeights", help="the method with which the muon scale variation histograms are derived")
     parser.add_argument("--scaleMuonCorr", type=float, default=1.0, help="Scale up/down dummy muon scale uncertainty by this factor")
     parser.add_argument("--correlatedNonClosureNuisances", action='store_true', help="get systematics from histograms for the Z non-closure nuisances without decorrelation in eta and pt")
-    parser.add_argument("--sepImpactForNC", action="store_true", help="use a dedicated impact gropu for non closure nuisances, instead of putting them in muonScale")
-    parser.add_argument("--sepImpactForReso", action="store_true", help="use a dedicated impact group for resolution correction nuisances")
     parser.add_argument("--noEfficiencyUnc", action='store_true', help="Skip efficiency uncertainty (useful for tests, because it's slow). Equivalent to --excludeNuisances '.*effSystTnP|.*effStatTnP' ")
     parser.add_argument("--effStatLumiScale", type=float, default=None, help="Rescale equivalent luminosity for efficiency stat uncertainty by this value (e.g. 10 means ten times more data from tag and probe)")
     parser.add_argument("--binnedScaleFactors", action='store_true', help="Use binned scale factors (different helpers and nuisances)")
@@ -591,7 +589,8 @@ def setup(args, inputFile, fitvar, xnorm=False):
     if non_closure_scheme in ["A-M-separated", "A-only"]:
         cardTool.addSystematic("Z_non_closure_parametrized_A", 
             processes=['single_v_samples'],
-            group="nonClosure" if args.sepImpactForNC else "muonCalibration",
+            group="nonClosure",
+            splitGroup={f"muonCalibration" : f".*"},
             baseName="Z_nonClosure_parametrized_A_",
             systAxes=["unc", "downUpVar"] if not correlated_non_closure else ["downUpVar"],
             labelsByAxis=["unc", "downUpVar"] if not correlated_non_closure else ["downUpVar"],
@@ -600,7 +599,8 @@ def setup(args, inputFile, fitvar, xnorm=False):
     if non_closure_scheme in ["A-M-separated", "M-only", "binned-plus-M"]:
         cardTool.addSystematic("Z_non_closure_parametrized_M", 
             processes=['single_v_samples'],
-            group="nonClosure" if args.sepImpactForNC else "muonCalibration",
+            group="nonClosure",
+            splitGroup={f"muonCalibration" : f".*"},
             baseName="Z_nonClosure_parametrized_M_",
             systAxes=["unc", "downUpVar"] if not correlated_non_closure else ["downUpVar"],
             labelsByAxis=["unc", "downUpVar"] if not correlated_non_closure else ["downUpVar"],
@@ -609,7 +609,8 @@ def setup(args, inputFile, fitvar, xnorm=False):
     if non_closure_scheme == "A-M-combined":
         cardTool.addSystematic("Z_non_closure_parametrized", 
             processes=['single_v_samples'],
-            group="nonClosure" if args.sepImpactForNC else "muonCalibration",
+            group="nonClosure",
+            splitGroup={f"muonCalibration" : f".*"},
             baseName="Z_nonClosure_parametrized_",
             systAxes=["unc", "downUpVar"] if not correlated_non_closure else ["downUpVar"],
             labelsByAxis=["unc", "downUpVar"] if not correlated_non_closure else ["downUpVar"],
@@ -618,7 +619,8 @@ def setup(args, inputFile, fitvar, xnorm=False):
     if non_closure_scheme in ["binned", "binned-plus-M"]:
         cardTool.addSystematic("Z_non_closure_binned", 
             processes=['single_v_samples'],
-            group="nonClosure" if args.sepImpactForNC else "muonCalibration",
+            group="nonClosure",
+            splitGroup={f"muonCalibration" : f".*"},
             baseName="Z_nonClosure_binned_",
             systAxes=["unc_ieta", "unc_ipt", "downUpVar"] if not correlated_non_closure else ["downUpVar"],
             labelsByAxis=["unc_ieta", "unc_ipt", "downUpVar"] if not correlated_non_closure else ["downUpVar"],
@@ -628,7 +630,8 @@ def setup(args, inputFile, fitvar, xnorm=False):
         cardTool.addSystematic("muonResolutionSyst_responseWeights", 
             mirror = True,
             processes=['single_v_samples'],
-            group="resolutionCrctn" if args.sepImpactForReso else "muonCalibration",
+            group="resolutionCrctn",
+            splitGroup={f"muonCalibration" : f".*"},
             baseName="Resolution_correction_",
             systAxes=["smearing_variation"],
             passToFakes=passSystToFakes
--- utilities/styles/styles.py
+++ utilities/styles/styles.py
@@ -5,7 +5,6 @@ common_groups = [
     "luminosity",
     "CMS_recoil",
     "CMS_background",
-    "muonScale",
     "theory_ew",
 ]
 nuisance_groupings = {
@@ -16,22 +15,17 @@ nuisance_groupings = {
         "resum",
         "muon_eff_syst",
         "muon_eff_stat",
-        "prefire"
+        "prefire",
+        "muonCalibration",
     ],
     "min": common_groups + [
         "massShiftW", "massShiftZ",
         "QCDscalePtChargeMiNNLO", "QCDscaleZPtChargeMiNNLO", "QCDscaleWPtChargeMiNNLO", "QCDscaleZPtHelicityMiNNLO", "QCDscaleWPtHelicityMiNNLO", "QCDscaleZPtChargeHelicityMiNNLO", "QCDscaleWPtChargeHelicityMiNNLO",
         "pdfMSHT20NoAlphaS", "pdfMSHT20AlphaS",
         "resumTNP", "resumNonpert", "resumTransition", "resumScale",
-        "muon_eff_stat_reco",
-        "muon_eff_stat_trigger",
-        "muon_eff_stat_iso",
-        "muon_eff_stat_idip",
-        "muon_eff_syst_reco",
-        "muon_eff_syst_trigger",
-        "muon_eff_syst_iso",
-        "muon_eff_syst_idip",
-        "muonPrefire",
-        "ecalPrefire"
+        "muon_eff_stat_reco", "muon_eff_stat_trigger", "muon_eff_stat_iso", "muon_eff_stat_idip",
+        "muon_eff_syst_reco", "muon_eff_syst_trigger", "muon_eff_syst_iso", "muon_eff_syst_idip",
+        "muonPrefire", "ecalPrefire",
+        "nonClosure", "resolutionCrctn",
     ]
 }
