f41601fa31e462087a23ce6f326156859edf3691
Josh Bendavid <Josh.Bendavid@cern.ch>
2023-11-10 08:32:11 -0500

    rework minnlo scale variations to be based on envelopes of helicity cross sections to smooth statistical fluctuations from per-event weights

--- scripts/histmakers/mw_with_mu_eta_pt.py
+++ scripts/histmakers/mw_with_mu_eta_pt.py
@@ -139,7 +139,7 @@ axis_recoWpt = hist.axis.Regular(40, 0., 80., name = "recoWpt", underflow=False,
 # define helpers
 muon_prefiring_helper, muon_prefiring_helper_stat, muon_prefiring_helper_syst = wremnants.make_muon_prefiring_helpers(era = era)
 
-qcdScaleByHelicity_helper = wremnants.makeQCDScaleByHelicityHelper()
+qcdScaleByHelicity_helper = wremnants.theory_corrections.make_qcd_uncertainty_helper_by_helicity()
 
 if args.noScaleFactors:
     logger.info("Running with no scale factors")
--- scripts/histmakers/mz_dilepton.py
+++ scripts/histmakers/mz_dilepton.py
@@ -98,7 +98,7 @@ if args.unfolding:
 # define helpers
 muon_prefiring_helper, muon_prefiring_helper_stat, muon_prefiring_helper_syst = wremnants.make_muon_prefiring_helpers(era = era)
 
-qcdScaleByHelicity_helper = wremnants.makeQCDScaleByHelicityHelper(is_w_like = True)
+qcdScaleByHelicity_helper = wremnants.theory_corrections.make_qcd_uncertainty_helper_by_helicity(is_w_like = True)
 
 # extra axes which can be used to label tensor_axes
 if args.binnedScaleFactors:
