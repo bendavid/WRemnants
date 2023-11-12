19bdebbeeefef178b25e2e3c536c2f4513266501
David <david.walter@cern.ch>
2023-11-12 11:51:36 +0100

    add uncertainties and further updates in postfit plotting

--- scripts/plotting/postfitPlots_combinetf2.py
+++ scripts/plotting/postfitPlots_combinetf2.py
@@ -3,9 +3,11 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 import os
+import hist
 import numpy as np
+import argparse
 
-from utilities import logging
+from utilities import logging, boostHistHelpers as hh
 from utilities.styles import styles
 from wremnants import plot_tools
 from utilities.io_tools import output_tools, combinetf2_input
@@ -39,7 +41,7 @@ logger = logging.setup_logger(__file__, 4 if args.debug else 3)
 
 outdir = output_tools.make_plot_dir(args.outpath, args.outfolder, eoscp=args.eoscp)
 
-fitmode = "prefit" if args.prefit else "postfit"
+fittype = "prefit" if args.prefit else "postfit"
 ratio = not args.noRatio
 data = not args.noData
 fitresult = combinetf2_input.get_fitresult(args.infile)
@@ -52,9 +54,8 @@ labels = [styles.process_labels.get(p, p) for p in procs]
 colors = [styles.process_colors.get(p, "grey") for p in procs]
 
 hists_data_obs = combinetf2_input.load_histograms(fitresult, "hist_data_obs", axes)
-hists_stack = combinetf2_input.load_histograms(fitresult, f"hist_{fitmode}", axes)
-
-pdb.set_trace()
+hists_stack = combinetf2_input.load_histograms(fitresult, f"hist_{fittype}", axes)
+hists_inclusive = combinetf2_input.load_histograms(fitresult, f"hist_{fittype}_inclusive", axes)
 
 translate_selection = {
     "charge": {
@@ -63,12 +64,12 @@ translate_selection = {
     }
 }
 
-def make_plot(h_data, h_stack, axis, suffix):
+def make_plot(h_data, h_inclusive, h_stack, axis_name, suffix):
 
     if ratio:
-        fig, ax1, ax2 = plot_tools.figureWithRatio(h_data, styles.xlabels.get(axis, "Bin number"), "Entries/bin", args.ylim, "Data/Pred.", args.rrange)
+        fig, ax1, ax2 = plot_tools.figureWithRatio(h_data, styles.xlabels.get(axis_name, "Bin number"), "Entries/bin", args.ylim, "Data/Pred.", args.rrange)
     else:
-        fig, ax1 = plot_tools.figure(h_data, styles.xlabels.get(axis, "Bin number"), "Entries/bin", args.ylim)
+        fig, ax1 = plot_tools.figure(h_data, styles.xlabels.get(axis_name, "Bin number"), "Entries/bin", args.ylim)
 
     hep.histplot(
         h_stack,
@@ -100,7 +101,7 @@ def make_plot(h_data, h_stack, axis, suffix):
     if ratio:
 
         hep.histplot(
-            hh.divideHists(sum(h_stack), sum(h_stack), cutoff=1e-8, rel_unc=True, flow=False, by_ax_name=False),
+            hh.divideHists(h_inclusive, h_inclusive, cutoff=1e-8, rel_unc=True, flow=False, by_ax_name=False),
             histtype="step",
             color="grey",
             alpha=0.5,
@@ -112,7 +113,7 @@ def make_plot(h_data, h_stack, axis, suffix):
 
         if data:
             hep.histplot(
-                hh.divideHists(h_data, sum(h_stack), cutoff=0.01, rel_unc=False),
+                hh.divideHists(h_data, h_inclusive, cutoff=0.01, rel_unc=False),
                 histtype="errorbar",
                 color="black",
                 label="Data",
@@ -121,13 +122,35 @@ def make_plot(h_data, h_stack, axis, suffix):
                 ax=ax2
             )
 
+            # for uncertaity bands
+            edges = h_inclusive.axes[0].edges
+
+            # need to divide by bin width
+            binwidth = edges[1:]-edges[:-1]
+            if h_inclusive.storage_type != hist.storage.Weight:
+                raise ValueError(f"Did not find uncertainties in {fittype} hist. Make sure you run combinetf with --computeHistErrors!")
+            nom = h_inclusive.values() / binwidth
+            std = np.sqrt(h_inclusive.variances()) / binwidth
+
+            hatchstyle = '///'
+            ax1.fill_between(edges, 
+                    np.append(nom+std, (nom+std)[-1]), 
+                    np.append(nom-std, (nom-std)[-1]),
+                step='post',facecolor="none", zorder=2, hatch=hatchstyle, edgecolor="k", linewidth=0.0, label="Uncertainty")
+
+            ax2.fill_between(edges, 
+                    np.append((nom+std)/nom, ((nom+std)/nom)[-1]), 
+                    np.append((nom-std)/nom, ((nom-std)/nom)[-1]),
+                step='post',facecolor="none", zorder=2, hatch=hatchstyle, edgecolor="k", linewidth=0.0)
+
     scale = max(1, np.divide(*ax1.get_figure().get_size_inches())*0.3)
     hep.cms.label(ax=ax1, lumi=float(f"{args.lumi:.3g}"), fontsize=20*args.scaleleg*scale, 
         label=args.cmsDecor, data=data)
 
     plot_tools.addLegend(ax1, ncols=2, text_size=20*args.scaleleg)
+    plot_tools.fix_axes(ax1, ax2, yscale=args.yscale)
 
-    to_join = [fitmode, args.postfix, axis, suffix]
+    to_join = [fittype, args.postfix, axis_name, suffix]
     outfile = "_".join(filter(lambda x: x, to_join))
 
     plot_tools.save_pdf_and_png(outdir, outfile)
@@ -143,7 +166,7 @@ def make_plot(h_data, h_stack, axis, suffix):
 
 # loop over channels (channels here correspond to the different histograms e.g. wmass & dilepton combined fit)
 nchannels=len(hists_data_obs)
-for i, (hist_data, hist_stack) in enumerate(zip(hists_data_obs, hists_stack)):
+for i, (hist_data, hist_inclusive, hist_stack) in enumerate(zip(hists_data_obs, hists_inclusive, hists_stack)):
     logger.info(f"Now at channel {i}")
     suffix = f"channel{i}"  if nchannels>1 else ""
 
@@ -154,9 +177,10 @@ for i, (hist_data, hist_stack) in enumerate(zip(hists_data_obs, hists_stack)):
 
         # make inclusive plot
         h_data = hist_data.project(axis)
+        h_inclusive = hist_inclusive.project(axis)
         h_stack = [h.project(axis) for h in hist_stack]
 
-        make_plot(h_data, h_stack, axis, suffix=f"{suffix}")
+        make_plot(h_data, h_inclusive, h_stack, axis, suffix=f"{suffix}")
 
         # loop over selections, make a plot for each bin
         for sa in [a for a in axes[i] if a.name in args.selectionAxes]:
@@ -165,7 +189,8 @@ for i, (hist_data, hist_stack) in enumerate(zip(hists_data_obs, hists_stack)):
                 logger.info(f"Make plot for axis {axis}, selection {sel}")
 
                 h_data = hist_data[{sa.name: idx}].project(axis)
+                h_inclusive = hist_inclusive[{sa.name: idx}].project(axis)
                 h_stack = [h[{sa.name: idx}].project(axis) for h in hist_stack]
 
-                make_plot(h_data, h_stack, axis, suffix=f"{suffix}{sel}")
+                make_plot(h_data, h_inclusive, h_stack, axis, suffix=f"{suffix}{sel}")
         
