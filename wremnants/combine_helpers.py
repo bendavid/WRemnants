6bd438be738a747c09544b1bd6fc34f8e42058b0
Jan Eysermans <janeysermans@gmail.com>
2023-11-15 08:21:19 -0500

    Remove old function

--- wremnants/combine_helpers.py
+++ wremnants/combine_helpers.py
@@ -34,39 +34,6 @@ def add_recoil_uncertainty(card_tool, samples, passSystToFakes=False, pu_type="h
             passToFakes=passSystToFakes,
         )
 
-def add_recoil_uncertainty__(card_tool, samples, passSystToFakes=False, pu_type="highPU", flavor="", group_compact=True):
-    met = input_tools.args_from_metadata(card_tool, "met")
-    if flavor == "":
-        flavor = input_tools.args_from_metadata(card_tool, "flavor")
-    if met == "RawPFMET":
-        rtag = f"{pu_type}_{flavor}_{met}"
-        if not rtag in recoil_tools.recoil_cfg:
-            logger.warning(f"Recoil corrections for {pu_type}, {flavor}, {met} not available.")
-            return
-        recoil_cfg = recoil_tools.recoil_cfg[rtag]
-        recoil_vars = list(recoil_cfg['corr_z'].keys()) + list(recoil_cfg['unc_z'].keys())
-        recoil_grps = recoil_vars
-        if group_compact:
-            recoil_grps = ["CMS_recoil"]*len(recoil_cfg)
-        for i, tag in enumerate(recoil_vars):
-            card_tool.addSystematic("recoilUnc_%s" % tag,
-                processes=samples,
-                mirror = False,
-                group = recoil_grps[i],
-                systAxes = ["recoilVar"],
-                passToFakes=passSystToFakes,
-            )
-    elif met == "DeepMETReso":
-        if input_tools.args_from_metadata(card_tool, "recoilUnc"):
-            tags = ["para", "perp"]
-            for i, tag in enumerate(tags):
-                card_tool.addSystematic("recoilUnc_%s" % tag,
-                    processes=samples,
-                    mirror = True,
-                    group = "recoil",
-                    systAxes = ["recoilVar"],
-                    passToFakes=passSystToFakes,
-                )
 
 def setSimultaneousABCD(cardTool,
     thresholdMT=40, integrateMT=False, axis_name_mt="mt", 
