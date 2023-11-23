c50bdd7d5651106d772190421fe07bdd5c7f69af
Elisabetta Manca <elisabetta.manca@cern.ch>
2023-11-23 10:00:43 -0500

    propagate helicity variations to OOA

--- scripts/combine/setupCombine.py
+++ scripts/combine/setupCombine.py
@@ -387,14 +387,15 @@ def setup(args, inputFile, fitvar, xnorm=False):
                                     processes=["signal_samples"],
                                     baseName=f"{nuisanceBaseNameOOA}{sign}_",
                                     noConstraint=True if args.priorNormXsec < 0 else False,
-                                    systAxes=["ptVgenSig"],
-                                    labelsByAxis=["PtVBin"],
+                                    systAxes=["ptVgenSig","helicitySig"],
+                                    formatWithValue=[None,"low"],
+                                    labelsByAxis=["PtVBin","Helicity"],
                                     systAxesFlow=["ptVgenSig"], # this can activate nuisances on overflow bins, mainly just ptV and yV since the helicity axis has no overflow bins
+                                    skipEntries=[{"helicitySig" : [6,7,8]}], # removing last three indices out of 9 (0,1,...,7,8) corresponding to A5,6,7
                                     actionMap={
                                         m.name: (lambda h: hh.addHists(h[{ax: hist.tag.Slicer()[::hist.sum] for ax in poi_axes}],
                                                                         h[{"ptVgenSig": hist.tag.Slicer()[hist.overflow:],
-                                                                            "absYVgenSig": hist.tag.Slicer()[0:h.axes["absYVgenSig"].size:hist.sum],
-                                                                            "helicitySig": hist.tag.Slicer()[::hist.sum]}],
+                                                                            "absYVgenSig": hist.tag.Slicer()[0:h.axes["absYVgenSig"].size:hist.sum]}],
                                                                         scale2=args.scaleNormXsecHistYields)
                                                     ) if sign in m.name else (lambda h: h[{ax: hist.tag.Slicer()[::hist.sum] for ax in poi_axes}]) for g in cardTool.procGroups["signal_samples"] for m in cardTool.datagroups.groups[g].members
                                     },
@@ -408,14 +409,15 @@ def setup(args, inputFile, fitvar, xnorm=False):
                                     processes=["signal_samples"],
                                     baseName=f"{nuisanceBaseNameOOA}{sign}_",
                                     noConstraint=True if args.priorNormXsec < 0 else False,
-                                    systAxes=["absYVgenSig"],
-                                    labelsByAxis=["YVBin"],
+                                    systAxes=["absYVgenSig","helicitySig"],
+                                    formatWithValue=[None,"low"],
+                                    labelsByAxis=["YVBin","Helicity"],
                                     systAxesFlow=["absYVgenSig"], # this can activate nuisances on overflow bins, mainly just ptV and yV since the helicity axis has no overflow bins
+                                    skipEntries=[{"helicitySig" : [6,7,8]}], # removing last three indices out of 9 (0,1,...,7,8) corresponding to A5,6,7
                                     actionMap={
                                         m.name: (lambda h: hh.addHists(h[{ax: hist.tag.Slicer()[::hist.sum] for ax in poi_axes}],
                                                                         h[{"ptVgenSig": hist.tag.Slicer()[0:h.axes["ptVgenSig"].size:hist.sum],
-                                                                            "absYVgenSig": hist.tag.Slicer()[hist.overflow:],
-                                                                            "helicitySig": hist.tag.Slicer()[::hist.sum]}],
+                                                                            "absYVgenSig": hist.tag.Slicer()[hist.overflow:]}],
                                                                         scale2=args.scaleNormXsecHistYields)
                                                     ) if sign in m.name else (lambda h: h[{ax: hist.tag.Slicer()[::hist.sum] for ax in poi_axes}]) for g in cardTool.procGroups["signal_samples"] for m in cardTool.datagroups.groups[g].members
                                     },
@@ -429,14 +431,15 @@ def setup(args, inputFile, fitvar, xnorm=False):
                                     processes=["signal_samples"],
                                     baseName=f"{nuisanceBaseNameOOA}{sign}_",
                                     noConstraint=True if args.priorNormXsec < 0 else False,
-                                    systAxes=["ptVgenSig", "absYVgenSig"],
-                                    labelsByAxis=["PtVBin", "YVBin"],
+                                    systAxes=["ptVgenSig", "absYVgenSig","helicitySig"],
+                                    formatWithValue=[None,None,"low"],
+                                    labelsByAxis=["PtVBin", "YVBin", "Helicity"],
                                     systAxesFlow=["ptVgenSig", "absYVgenSig"], # this can activate nuisances on overflow bins, mainly just ptV and yV since the helicity axis has no overflow bins
+                                    skipEntries=[{"helicitySig" : [6,7,8]}], # removing last three indices out of 9 (0,1,...,7,8) corresponding to A5,6,7
                                     actionMap={
                                         m.name: (lambda h: hh.addHists(h[{ax: hist.tag.Slicer()[::hist.sum] for ax in poi_axes}],
                                                                         h[{"ptVgenSig": hist.tag.Slicer()[hist.overflow:],
-                                                                            "absYVgenSig": hist.tag.Slicer()[hist.overflow:],
-                                                                            "helicitySig": hist.tag.Slicer()[::hist.sum]}],
+                                                                            "absYVgenSig": hist.tag.Slicer()[hist.overflow:]}],
                                                                         scale2=args.scaleNormXsecHistYields)
                                                     ) if sign in m.name else (lambda h: h[{ax: hist.tag.Slicer()[::hist.sum] for ax in poi_axes}]) for g in cardTool.procGroups["signal_samples"] for m in cardTool.datagroups.groups[g].members
                                     },
