1ec657aed3fb4ee8971af4522b72d11c89d1ac93
David <david.walter@cern.ch>
2023-11-24 15:03:00 +0100

    Updates

--- scripts/tests/dilepton_modeling_bias_study.sh
+++ scripts/tests/dilepton_modeling_bias_study.sh
@@ -1,16 +1,27 @@
 HISTMAKER_FILE=$1
 COMBINE_OUTDIR=$2
+ANALYSIS=ZMassWLike
 
 CMSSW_BASE=/home/d/dwalter/CMSSW_10_6_30/src
 
-LABEL=Z
-ANALYSIS=ZMassDilepton
-WEBDIR=231123_dilepton_biasstudy/ptLow
-
 PSEUDODATA=("uncorr" "dyturboN3LLpCorr" "dyturboN3LLp2dCorr" "matrix_radishCorr" "scetlibNPCorr" "scetlibN4LLCorr")
-SETS=("asimov" "data" ${PSEUDODATA[@]})
 
-FITVARS=( "ptll" "ptll-yll" )
+PROJECT=231123_dilepton_biasstudy
+
+if [ "${ANALYSIS}" == "ZMassDilepton" ]; then
+    WEBDIR=${PROJECT}/dilepton
+    FITVARS=( "ptll" "ptll-yll" )
+    SETS=("asimov" "data" ${PSEUDODATA[@]})
+elif [ "${ANALYSIS}" == "ZMassWLike" ]; then
+    WEBDIR=${PROJECT}/wlike
+    FITVARS=( "eta-pt-charge" )
+    SETS=("asimov" ${PSEUDODATA[@]})
+elif [ "${ANALYSIS}" == "WMass" ]; then
+    WEBDIR=${PROJECT}/wmass
+    FITVARS=( "eta-pt-charge" )
+    SETS=("asimov" ${PSEUDODATA[@]})
+fi
+
 for FITVAR in "${FITVARS[@]}"; do
     echo Run fits for: ${FITVAR}
 
@@ -24,7 +35,7 @@ for FITVAR in "${FITVARS[@]}"; do
     else
         echo "The file $COMBINE_ANALYSIS_PATH does not exists, continue using it."
         COMMAND="./scripts/ci/run_with_singularity.sh scripts/ci/setup_and_run_python.sh scripts/combine/setupCombine.py \
-        -i $HISTMAKER_FILE --fitvar $FITVAR --axlim 0 27 \
+        -i $HISTMAKER_FILE --fitvar $FITVAR \
         -o $COMBINE_OUTDIR --hdf5 --realData --pseudoData ${PSEUDODATA[@]}"
         echo Run command: $COMMAND
         eval $COMMAND
@@ -58,22 +69,29 @@ for FITVAR in "${FITVARS[@]}"; do
             $COMBINE_ANALYSIS_OUTDIR/fitresults_123456789_${PSEUDO}.root -f ${WEBDIR}/$PSEUDO --yscale '1.2' --rrange '0.99' '1.01'
         if [ "${PSEUDO}" != "asimov" ]; then
             echo "Make impacts for ${PSEUDO}"
-            ./scripts/ci/run_with_singularity.sh scripts/ci/setup_and_run_python.sh scripts/combine/pullsAndImpacts.py \
-                -r $COMBINE_ANALYSIS_OUTDIR/fitresults_123456789_asimov.hdf5 -f $COMBINE_ANALYSIS_OUTDIR/fitresults_123456789_${PSEUDO}.hdf5 \
-                -m ungrouped --sortDescending -s constraint \
-                output --outFolder /home/d/dwalter/www/WMassAnalysis/${WEBDIR}/$PSEUDO/ -o impactsWlike_${FITVARSTING}.html --otherExtensions pdf png -n 50 
-                # --oneSidedImpacts --grouping max -t utilities/styles/nuisance_translate.json \
+            if [ "${ANALYSIS}" == "ZMassDilepton" ]; then
+                ./scripts/ci/run_with_singularity.sh scripts/ci/setup_and_run_python.sh scripts/combine/pullsAndImpacts.py \
+                    -r $COMBINE_ANALYSIS_OUTDIR/fitresults_123456789_asimov.hdf5 -f $COMBINE_ANALYSIS_OUTDIR/fitresults_123456789_${PSEUDO}.hdf5 \
+                    -m ungrouped --sortDescending -s constraint \
+                    output --outFolder /home/d/dwalter/www/WMassAnalysis/${WEBDIR}/$PSEUDO/ -o impacts${ANALYSIS}_${FITVARSTING}.html --otherExtensions pdf png -n 50 
+                    # --oneSidedImpacts --grouping max -t utilities/styles/nuisance_translate.json \
+            else
+                ./scripts/ci/run_with_singularity.sh scripts/ci/setup_and_run_python.sh scripts/combine/pullsAndImpacts.py \
+                    -r $COMBINE_ANALYSIS_OUTDIR/fitresults_123456789_asimov.hdf5 -f $COMBINE_ANALYSIS_OUTDIR/fitresults_123456789_${PSEUDO}.hdf5 \
+                    --oneSidedImpacts --grouping max -t utilities/styles/nuisance_translate.json \
+                    output --outFolder /home/d/dwalter/www/WMassAnalysis/${WEBDIR}/$PSEUDO/ -o impacts${ANALYSIS}_${FITVARSTING}.html --otherExtensions pdf png -n 50 
+            fi
         fi
     done
 
-    # # 4) make summary table
-    # ./scripts/ci/run_with_singularity.sh scripts/ci/setup_and_run_python.sh scripts/tests/summarytable.py \
-    #     $COMBINE_ANALYSIS_OUTDIR/fitresults_123456789_*.root -f ${WEBDIR}
-    # pdflatex -output-directory /home/d/dwalter/www/WMassAnalysis/${WEBDIR}/ /home/d/dwalter/www/WMassAnalysis/${WEBDIR}/table_${ANALYSIS}.tex
+    # 4) make summary table
+    ./scripts/ci/run_with_singularity.sh scripts/ci/setup_and_run_python.sh scripts/tests/summarytable.py \
+        $COMBINE_ANALYSIS_OUTDIR/fitresults_123456789_*.root -f ${WEBDIR}
+    pdflatex -output-directory /home/d/dwalter/www/WMassAnalysis/${WEBDIR}/ /home/d/dwalter/www/WMassAnalysis/${WEBDIR}/table_${ANALYSIS}.tex
 done
 
 # 4) make big summary table
-# ./scripts/ci/run_with_singularity.sh scripts/ci/setup_and_run_python.sh scripts/tests/summarytable.py \
-#     ${COMBINE_OUTDIR}/${ANALYSIS}_*/fitresults_123456789_*.root -f ${WEBDIR}
+./scripts/ci/run_with_singularity.sh scripts/ci/setup_and_run_python.sh scripts/tests/summarytable.py \
+    ${COMBINE_OUTDIR}/${ANALYSIS}_*/fitresults_123456789_*.root -f ${WEBDIR}
 
-# pdflatex -output-directory /home/d/dwalter/www/WMassAnalysis/${WEBDIR}/ /home/d/dwalter/www/WMassAnalysis/${WEBDIR}/table_${ANALYSIS}.tex
+pdflatex -output-directory /home/d/dwalter/www/WMassAnalysis/${WEBDIR}/ /home/d/dwalter/www/WMassAnalysis/${WEBDIR}/table_${ANALYSIS}.tex
