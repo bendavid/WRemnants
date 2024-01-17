Shift mw in corrections from winhac/horace samples by -30MeV according to wrong value in samples; small fixes

scripts/corrections/make_theory_corr_ew.py
@@ -44,7 +44,6 @@ res, meta, _ = input_tools.read_infile(args.input)
 corrh = {}
 
 for proc in procs:
-
     # Make 2D ratio
     logger.info(f'Make 2D ratio for {proc}')
     def prepare_hist(name):
@@ -67,16 +66,27 @@ for proc in procs:
             logger.info(f'Integral for {name} after scaling {np.sum(histo.values(flow=True))}')
 
         if "ewMll" in histo.axes.name:
-            rebinN=4 # make one bin out of 4
             edges = histo.axes["ewMll"].edges
-            if proc[0] == "Z":
-                # keep low and high bin edges of [0,50,60, ... 120] for technical reason
-                rebin_edges = np.append(np.append(edges[:2],edges[2:-1][::rebinN]),edges[-1:])
-            else:
-                rebin_edges = edges[::rebinN]
 
-            logger.info(f"Rebin axis ewMll by {rebinN}")
-            histo = hh.rebinHist(histo, "ewMll", rebin_edges)
+            if proc[0] == "W" and ("horace" in name or "winhac" in name):
+                logger.warning("horace and winhac samples with version <v5 have a wrong wmass by 30MeV too high, move axis edges by 30MeV")
+                edges = edges-0.03
+
+                axes = [hist.axis.Variable(edges, underflow=False, name='ewMll') if ax.name == "ewMll" else ax for ax in histo.axes]
+                new_histo = hist.Hist(*axes, storage=histo.storage_type())
+                new_histo.view(flow=True)[...] = histo.view(flow=True)
+                histo = new_histo
+
+            rebinN=4 # make one bin out of rebinN
+            if rebinN != 1:
+                if proc[0] == "Z":
+                    # keep low and high bin edges of [0,50,60, ... 120] for technical reason
+                    rebin_edges = np.append(np.append(edges[:2],edges[2:-1][::rebinN]),edges[-1:])
+                else:
+                    rebin_edges = edges[::rebinN]
+
+                logger.info(f"Rebin axis ewMll by {rebinN}")
+                histo = hh.rebinHist(histo, "ewMll", rebin_edges)
 
         return histo
     
@@ -156,9 +166,9 @@ for proc in procs:
 
 for name, corr_dict in corrh.items():
     outname = name.replace('-', '')
-    outfile = f"{args.outpath}/{outname}"
     if args.postfix:
         outname += f"_{args.postfix}"
+    outfile = f"{args.outpath}/{outname}"
 
     if 'Zmumu' in corr_dict:
         output_tools.write_theory_corr_hist(outfile, 'Z', 