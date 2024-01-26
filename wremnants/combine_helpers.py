f8f37979a7d53ee38891645f206d4fdc21889780
David <david.walter@cern.ch>
2024-01-26 17:24:28 +0100

    Bugfix

--- wremnants/combine_helpers.py
+++ wremnants/combine_helpers.py
@@ -45,7 +45,7 @@ def add_electroweak_uncertainty(card_tool, ewUncs, flavor="mu", samples="single_
     for ewUnc in ewUncs:
         if ewUnc == "default":
             if z_samples:
-                if mode=="wmass" or mode=="wlike":
+                if mode in ["wmass", "wlike", "lowpu_w"]:
                     ewUnc = "virtual_ew_wlike"
                 else:
                     ewUnc = "virtual_ew"
