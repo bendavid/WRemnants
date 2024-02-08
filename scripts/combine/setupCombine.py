b44e8722b08a0a880a319b8090658c20ed9c27ce
Marco Cipriani <marco.cipriani@cern.ch>
2024-02-08 10:54:39 +0100

    fix for muon veto when not doing Wmass and highPU

--- scripts/combine/setupCombine.py
+++ scripts/combine/setupCombine.py
@@ -72,7 +72,7 @@ def make_parser(parser=None):
     parser.add_argument("--effStatLumiScale", type=float, default=None, help="Rescale equivalent luminosity for efficiency stat uncertainty by this value (e.g. 10 means ten times more data from tag and probe)")
     parser.add_argument("--binnedScaleFactors", action='store_true', help="Use binned scale factors (different helpers and nuisances)")
     parser.add_argument("--isoEfficiencySmoothing", action='store_true', help="If isolation SF was derived from smooth efficiencies instead of direct smoothing")
-    parser.add_argument("--scaleZmuonVeto", default=1, type=float, help="Scale the second muon veto uncertainties by this factor")
+    parser.add_argument("--scaleZmuonVeto", default=1, type=float, help="Scale the second muon veto uncertainties by this factor for Wmass")
     # pseudodata
     parser.add_argument("--pseudoData", type=str, nargs="+", help="Histograms to use as pseudodata")
     parser.add_argument("--pseudoDataAxes", type=str, nargs="+", default=[None], help="Variation axes to use as pseudodata for each of the histograms")
@@ -132,9 +132,6 @@ def setup(args, inputFile, fitvar, xnorm=False):
 
     simultaneousABCD = wmass and args.ABCD and not xnorm
     constrainMass = (dilepton and not "mll" in fitvar) or args.fitXsec
-
-    if not wmass and args.scaleZmuonVeto:
-        raise ValueError("Option --scaleZmuonVeto was specified, but it only applies to wmass analysis. Please check")
 
     if wmass:
         base_group = "Wenu" if datagroups.flavor == "e" else "Wmunu"
@@ -627,22 +624,24 @@ def setup(args, inputFile, fitvar, xnorm=False):
                                 systAxes=["downUpVar"],
                                 labelsByAxis=["downUpVar"],
                                 passToFakes=passSystToFakes)
-        decorrVetoDict = {
-            "x" : {
-                "label" : "eta",
-                "edges": [round(-2.4+i*0.1,1) for i in range(49)]
+        ## TODO: implement second lepton veto for low PU (both electrons and muons)
+        if not lowPU:
+            decorrVetoDict = {
+                "x" : {
+                    "label" : "eta",
+                    "edges": [round(-2.4+i*0.1,1) for i in range(49)]
+                }
             }
-        }
-        cardTool.addSystematic("ZmuonVeto",
-                               processes=['Zveto_samples'],
-                               group="ZmuonVeto",
-                               mirror=True,
-                               systAxes=[],
-                               outNames=["ZmuonVetoDown", "ZmuonVetoUp"],
-                               passToFakes=passSystToFakes,
-                               scale=args.scaleZmuonVeto,
-                               decorrelateByBin=decorrVetoDict
-                               )
+            cardTool.addSystematic("ZmuonVeto",
+                                   processes=['Zveto_samples'],
+                                   group="ZmuonVeto",
+                                   mirror=True,
+                                   systAxes=[],
+                                   outNames=["ZmuonVetoDown", "ZmuonVetoUp"],
+                                   passToFakes=passSystToFakes,
+                                   scale=args.scaleZmuonVeto,
+                                   decorrelateByBin=decorrVetoDict
+                                   )
     else:
         cardTool.addLnNSystematic("CMS_background", processes=["Other"], size=1.15, group="CMS_background")
         cardTool.addLnNSystematic("luminosity", processes=['MCnoQCD'], size=1.017 if lowPU else 1.012, group="luminosity")
